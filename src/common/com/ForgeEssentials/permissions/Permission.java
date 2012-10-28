332dabdce86b2edded280b7b08683f3307a98647
Abrar Syed <sacabrarsyed@gmail.com>
2012-10-28 09:15:17 -0500

    fixed up packets

--- src/common/com/ForgeEssentials/permissions/Permission.java
+++ src/common/com/ForgeEssentials/permissions/Permission.java
@@ -4,10 +4,15 @@ import net.minecraft.src.EntityPlayer;
 import net.minecraftforge.event.Event;
 import net.minecraftforge.event.Event.HasResult;
 
+
+/**
+ * @author AbrarSyed
+ * This class is pretty much abstract. don't instantiate. its useless.
+ */
 @HasResult
 public class Permission extends Event
 {
-	public Permission(EntityPlayer player)
+	public Permission()
 	{
 		super();
 	}
--- src/minecraft/com/ForgeEssentials/network/HandlerClient.java
+++ src/minecraft/com/ForgeEssentials/network/HandlerClient.java
@@ -1,5 +1,10 @@
 package com.ForgeEssentials.network;
 
+import java.nio.ByteBuffer;
+
+import com.ForgeEssentials.WorldControl.ExtendedPlayerControllerMP;
+import com.ForgeEssentials.WorldControl.WorldControlMain;
+
 import net.minecraft.src.INetworkManager;
 import net.minecraft.src.Packet250CustomPayload;
 import cpw.mods.fml.common.network.IPacketHandler;
@@ -10,6 +15,12 @@ public class HandlerClient implements IPacketHandler
 	@Override
 	public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player)
 	{
+		if(packet.channel.equals(WorldControlMain.CHANNEL))
+		{
+			ByteBuffer bb = ByteBuffer.allocate(4);
+			bb.put(packet.data);
+			ExtendedPlayerControllerMP.reachDistance = bb.getFloat(0);
+		}
 
 	}
 }
