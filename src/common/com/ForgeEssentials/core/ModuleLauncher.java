05fec5670999d5b1f0cf1005f09bb55a58129322
Bob-A-Red-Dino <bob.a.red.dino@gmail.com>
2012-11-18 17:31:03 -0500

    Fixed conflicts from last commit

--- src/common/com/ForgeEssentials/core/ModuleLauncher.java
+++ src/common/com/ForgeEssentials/core/ModuleLauncher.java
@@ -5,9 +5,6 @@ import com.ForgeEssentials.commands.ModuleCommands;
 import com.ForgeEssentials.core.commands.CoreCommands;
 import com.ForgeEssentials.permissions.ModulePermissions;
 
-import cpw.mods.fml.common.Mod.Init;
-import cpw.mods.fml.common.Mod.PreInit;
-import cpw.mods.fml.common.Mod.ServerStarting;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
