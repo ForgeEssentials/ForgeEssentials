ec823eb28dc13fb5d0a94c80e048d0f39fd6b1d6
Lucas Tan <luacs1998@live.com>
2012-11-30 11:54:07 +0800

    If there are any kind Samaritans out there that can fix this...

--- src/common/com/ForgeEssentials/commands/CommandRestart.java
+++ src/common/com/ForgeEssentials/commands/CommandRestart.java
@@ -2,14 +2,12 @@ package com.ForgeEssentials.commands;
 
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.ICommandSender;
-import net.minecraft.src.ServerConfigurationManager;
 
 import com.ForgeEssentials.core.commands.ForgeEssentialsCommandBase;
+import com.ForgeEssentials.util.OutputHandler;
 
 public class CommandRestart extends ForgeEssentialsCommandBase
 {
-
-	private ServerConfigurationManager serverconfmgr;
 
 	@Override
 	public String getCommandName()
@@ -25,16 +23,38 @@ public class CommandRestart extends ForgeEssentialsCommandBase
 	@Override
 	public void processCommandConsole(ICommandSender sender, String[] args)
 	{
-		sender.sendChatToPlayer("Not yet implemented");
+		OutputHandler.SOP("Not yet implemented, check back soon!");
 		/**
-		 * Not implemented sender.sendChatToPlayer("Restarting server..."); if (MinecraftServer.getServer().getNetworkThread() != null) { MinecraftServer.getServer().getNetworkThread().stopListening(); }
-		 * 
-		 * if (serverconfmgr != null) { sender.sendChatToPlayer("Saving players"); serverconfmgr.saveAllPlayerData(); serverconfmgr.removeAllPlayers(); }
-		 * 
-		 * sender.sendChatToPlayer("Saving worlds"); SaveUtil.saveGame(); WorldServer[] var1 = MinecraftServer.getServer().worldServers; int var2 = var1.length;
-		 * 
-		 * for (int var3 = 0; var3 < var2; ++var3) { WorldServer var4 = var1[var3]; MinecraftForge.EVENT_BUS.post(new WorldEvent.Unload(var4)); var4.flush(); DimensionManager.setWorld(var4.provider.dimensionId, null); } FMLRelauncher.handleServerRelaunch(new ArgsWrapper(args));
-		 */
+		try {
+            
+            File file = new File(startupScript);
+            if (file.isFile()) {
+                System.out.println("Attempting to restart with " + startupScript);
+
+                // Kick all players
+                
+                //Close the socket
+
+                // Give the socket a chance to send the packets
+                try {
+                    MinecraftServer.getServer().stopServer();
+                } catch (Throwable t) {
+                }
+
+                String os = System.getProperty("os.name").toLowerCase();
+                if (os.contains("win")) {
+                    Runtime.getRuntime().exec("cmd /c start " + file.getPath());
+                } else {
+                    Runtime.getRuntime().exec(file.getPath());
+                }
+                System.exit(0);
+            } else {
+                System.out.println("Startup script '" + startupScript + "' does not exist! Ensure a shell script named restart.bat or restart.sh is placed in the ForgeEssentials folder!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        */
 	}
 
 	@Override
