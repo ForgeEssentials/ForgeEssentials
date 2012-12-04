f0741a8bb17ef52029ce0a91cc539d5f15b1b185
Bob-A-Red-Dino <bob.a.red.dino@gmail.com>
2012-12-04 16:31:47 -0500

    Console use for /butcher and /remove

--- src/common/com/ForgeEssentials/commands/CommandRemove.java
+++ src/common/com/ForgeEssentials/commands/CommandRemove.java
@@ -3,8 +3,10 @@ package com.ForgeEssentials.commands;
 import java.util.List;
 
 import com.ForgeEssentials.core.commands.ForgeEssentialsCommandBase;
+import com.ForgeEssentials.util.FunctionHelper;
 import com.ForgeEssentials.util.Localization;
 import com.ForgeEssentials.util.OutputHandler;
+import com.ForgeEssentials.util.AreaSelector.WorldPoint;
 
 import net.minecraft.src.AxisAlignedBB;
 import net.minecraft.src.EntityItem;
@@ -24,9 +26,9 @@ public class CommandRemove extends ForgeEssentialsCommandBase
 	public void processCommandPlayer(EntityPlayer sender, String[] args)
 	{
 		int radius = 10;
-		double centerX = sender.posX;
-		double centerY = sender.posY;
-		double centerZ = sender.posZ;
+		int centerX = (int) sender.posX;
+		int centerY = (int) sender.posY;
+		int centerZ = (int) sender.posZ;
 
 		if (args.length == 1)
 		{
@@ -48,21 +50,21 @@ public class CommandRemove extends ForgeEssentialsCommandBase
 			}
 			try
 			{
-				centerX = Double.parseDouble(args[1]);
+				centerX = Integer.parseInt(args[1]);
 			} catch (NumberFormatException e)
 			{
 				OutputHandler.chatError(sender, Localization.format(Localization.ERROR_NAN, args[1]));
 			}
 			try
 			{
-				centerY = Double.parseDouble(args[2]);
+				centerY = Integer.parseInt(args[2]);
 			} catch (NumberFormatException e)
 			{
 				OutputHandler.chatError(sender, Localization.format(Localization.ERROR_NAN, args[2]));
 			}
 			try
 			{
-				centerZ = Double.parseDouble(args[3]);
+				centerZ = Integer.parseInt(args[3]);
 			} catch (NumberFormatException e)
 			{
 				OutputHandler.chatError(sender, Localization.format(Localization.ERROR_NAN, args[3]));
@@ -79,18 +81,76 @@ public class CommandRemove extends ForgeEssentialsCommandBase
 			entity.setDead();
 		}
 		OutputHandler.chatConfirmation(sender, Localization.format(Localization.REMOVED, counter));
-
 	}
 
 	@Override
 	public void processCommandConsole(ICommandSender sender, String[] args)
 	{
+		int radius = 0;
+		WorldPoint center = new WorldPoint(0, 0, 0, 0);
+
+		if (args.length >= 4)
+		{
+			try
+			{
+				radius = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e)
+			{
+				sender.sendChatToPlayer(Localization.format(Localization.ERROR_NAN, args[0]));
+			}
+			try
+			{
+				center.x = Integer.parseInt(args[1]);
+			} catch (NumberFormatException e)
+			{
+				sender.sendChatToPlayer(Localization.format(Localization.ERROR_NAN, args[1]));
+			}
+			try
+			{
+				center.y = Integer.parseInt(args[2]);
+			} catch (NumberFormatException e)
+			{
+				sender.sendChatToPlayer(Localization.format(Localization.ERROR_NAN, args[2]));
+			}
+			try
+			{
+				center.z = Integer.parseInt(args[3]);
+			} catch (NumberFormatException e)
+			{
+				sender.sendChatToPlayer(Localization.format(Localization.ERROR_NAN, args[3]));
+			}
+			if (args.length >= 5)
+			{
+				try
+				{
+					center.dim = Integer.parseInt(args[3]);
+				} catch (NumberFormatException e)
+				{
+					sender.sendChatToPlayer(Localization.format(Localization.ERROR_NAN, args[3]));
+				}
+			}
+		} else
+		{
+			sender.sendChatToPlayer(Localization.get(Localization.ERROR_BADSYNTAX));
+			return;
+		}
+
+		List<EntityItem> entityList = (List<EntityItem>) FunctionHelper.getDimension(center.dim).getEntitiesWithinAABB(EntityItem.class, AxisAlignedBB.getAABBPool().addOrModifyAABBInPool(center.x - radius, center.y - radius, center.z - radius, center.x + radius + 1, center.y + radius + 1, center.z + radius + 1));
+
+		int counter = 0;
+		for (int i = 0; i < entityList.size(); i++)
+		{
+			EntityItem entity = entityList.get(i);
+			counter++;
+			entity.setDead();
+		}
+		sender.sendChatToPlayer(Localization.format(Localization.REMOVED, counter));
 	}
 
 	@Override
 	public boolean canConsoleUseCommand()
 	{
-		return false;
+		return true;
 	}
 
 	@Override
