b0cafa4e6ac012724c8325721ec8185e9348874d
AbrarSyed <sacabrarsyed@gmail.com>
2012-12-05 22:53:10 -0600

    PERMISSIONS ARE BACK TO LIFE!!!!

--- src/common/com/ForgeEssentials/coremod/Downloader.java
+++ src/common/com/ForgeEssentials/coremod/Downloader.java
@@ -4,23 +4,27 @@ import cpw.mods.fml.relauncher.ILibrarySet;
 
 //Kindly do not reference any FE classes outside the coremod package in this class.
 
-public class Downloader implements ILibrarySet{
+public class Downloader implements ILibrarySet
+{
 
-	private static String[] libraries = { "WorldEdit.jar", "WorldGuard.jar", "mysql-connector-java-bin.jar" };
-	private static String[] checksums = { "2190c96afbf717a01d0cdceaa772866cd1794c45", "c5f93238788238da399ec0ed1eed3e0c3af54912", "3ae0cff91d7f40d5b4c7cefbbd1eab34025bdc15"};
+	private static String[]	libraries	= { "WorldEdit.jar", "WorldGuard.jar", "mysql-connector-java-bin.jar" };
+	private static String[]	checksums	= { "2190c96afbf717a01d0cdceaa772866cd1794c45", "c5f93238788238da399ec0ed1eed3e0c3af54912", "3ae0cff91d7f40d5b4c7cefbbd1eab34025bdc15" };
 
 	@Override
-	public String[] getLibraries() {
+	public String[] getLibraries()
+	{
 		return libraries;
 	}
 
 	@Override
-	public String[] getHashes() {
+	public String[] getHashes()
+	{
 		return checksums;
 	}
 
 	@Override
-	public String getRootURL() {
+	public String getRootURL()
+	{
 		return "https://github.com/ForgeEssentials/ForgeEssentialsMain/raw/master/lib/%s";
 	}
 
--- src/common/com/ForgeEssentials/coremod/FEModContainer.java
+++ src/common/com/ForgeEssentials/coremod/FEModContainer.java
@@ -10,25 +10,28 @@ import cpw.mods.fml.common.ModMetadata;
 
 // Kindly do not reference any FE classes outside the coremod package in this class.
 
-public class FEModContainer extends DummyModContainer {
-        
-	public boolean mod;
-	
-	public FEModContainer() {
-                super(new ModMetadata());
-                /* ModMetadata is the same as mcmod.info */
-                ModMetadata myMeta = super.getMetadata();
-                myMeta.authorList = Arrays.asList(new String[] { "See the ForgeEssentials author list." });
-                myMeta.description = "The preloader for Forge Essentials.";
-                myMeta.modId = "FEPreLoader";
-                myMeta.version = "0.1.0";
-                myMeta.name = "Forge Essentials|PreLoader";
-                myMeta.url = "";
-        }
-        
-        public boolean registerBus(EventBus bus, LoadController controller) {
-        bus.register(this);
-        return true;
-        }
-        
+public class FEModContainer extends DummyModContainer
+{
+
+	public boolean	mod;
+
+	public FEModContainer()
+	{
+		super(new ModMetadata());
+		/* ModMetadata is the same as mcmod.info */
+		ModMetadata myMeta = super.getMetadata();
+		myMeta.authorList = Arrays.asList(new String[] { "See the ForgeEssentials author list." });
+		myMeta.description = "The preloader for Forge Essentials.";
+		myMeta.modId = "FEPreLoader";
+		myMeta.version = "0.1.0";
+		myMeta.name = "Forge Essentials|PreLoader";
+		myMeta.url = "";
+	}
+
+	public boolean registerBus(EventBus bus, LoadController controller)
+	{
+		bus.register(this);
+		return true;
+	}
+
 }
