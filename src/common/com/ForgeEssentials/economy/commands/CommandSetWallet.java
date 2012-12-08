4d9d09ff100e571024335e59fef8899cfb7b8f74
Nik <hobos_taco@hotmail.com>
2012-12-08 16:06:51 +1000

    Economy Initial Commit
    External mods should only call methods from the Wallet class.

---
+++ src/common/com/ForgeEssentials/economy/commands/CommandSetWallet.java
@@ -0,0 +1,93 @@
+package com.ForgeEssentials.economy.commands;
+
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.ICommandSender;
+import com.ForgeEssentials.core.commands.ForgeEssentialsCommandBase;
+import com.ForgeEssentials.economy.Wallet;
+import com.ForgeEssentials.util.Localization;
+import com.ForgeEssentials.util.OutputHandler;
+import cpw.mods.fml.common.FMLCommonHandler;
+
+public class CommandSetWallet extends ForgeEssentialsCommandBase
+{
+	@Override
+	public String getCommandName()
+	{
+		return Localization.get(Localization.WALLET_SET);
+	}
+
+	@Override
+	public void processCommandPlayer(EntityPlayer sender, String[] args)
+	{
+		if (args.length == 2)
+		{
+			EntityPlayer player = FMLCommonHandler.instance().getSidedDelegate().getServer().getConfigurationManager().getPlayerForUsername(args[0]);
+			int amountToSet = Integer.parseInt(args[1]);
+			
+			if (player == null)
+			{
+				OutputHandler.chatError(sender, (Localization.get(Localization.ERROR_NOPLAYER)));
+			}
+			else
+			{
+				Wallet.setWallet(amountToSet, player);
+				
+				if (sender != player)
+				{
+					sender.sendChatToPlayer(Localization.get(Localization.WALLET_SET_TARGET) + Wallet.getWallet(player) + " " + Wallet.currency(Wallet.getWallet(player)));
+				}
+				player.sendChatToPlayer(Localization.get(Localization.WALLET_SET_SELF) + Wallet.getWallet(player) + " " + Wallet.currency(Wallet.getWallet(player)));
+			}
+		} 
+		else
+		{
+			OutputHandler.chatError(sender, (Localization.get(Localization.ERROR_BADSYNTAX) + getSyntaxPlayer(sender)));
+		}
+
+	}
+
+	@Override
+	public void processCommandConsole(ICommandSender sender, String[] args)
+	{
+		if (args.length == 2)
+		{
+			EntityPlayer player = FMLCommonHandler.instance().getSidedDelegate().getServer().getConfigurationManager().getPlayerForUsername(args[0]);
+			int amountToSet = Integer.parseInt(args[1]);
+			
+			if (player == null)
+			{
+				sender.sendChatToPlayer(Localization.get(Localization.ERROR_NOPLAYER));
+			}
+			else
+			{
+				Wallet.setWallet(amountToSet, player);
+				
+				sender.sendChatToPlayer(Localization.get(Localization.WALLET_SET_TARGET) + Wallet.getWallet(player) + " " + Wallet.currency(Wallet.getWallet(player)));
+				player.sendChatToPlayer(Localization.get(Localization.WALLET_SET_SELF) + Wallet.getWallet(player) + " " + Wallet.currency(Wallet.getWallet(player)));
+			}
+		} 
+		else
+		{
+			sender.sendChatToPlayer(Localization.get(Localization.ERROR_BADSYNTAX) + getSyntaxConsole());
+		}
+	}
+
+	@Override
+	public boolean canConsoleUseCommand()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean canPlayerUseCommand(EntityPlayer player)
+	{
+		return true;
+	}
+
+	@Override
+	public String getCommandPerm()
+	{
+		return "ForgeEssentials.Economy." + getCommandName();
+	}
+
+}
