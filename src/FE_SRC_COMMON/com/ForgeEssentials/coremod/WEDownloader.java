62052bcbf4c24e460d9a5fe1d369c111bc9226f8
Lucas Tan <luacs1998@live.com>
2012-12-19 09:59:41 +0800

    They see me downloading files, they hatin'. Anyway, fixed

--- src/FE_SRC_COMMON/com/ForgeEssentials/coremod/WEDownloader.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/coremod/WEDownloader.java
@@ -5,7 +5,7 @@ import cpw.mods.fml.relauncher.ILibrarySet;
 public class WEDownloader implements ILibrarySet{
 
 	private static String[] we = {"worldedit-5.4.6-SNAPSHOT.jar"};
-	private static String[] wehash = {"897c73e7864b4a130933c64f138b8cf5547792ac"};
+	private static String[] wehash = {"de96549f9c31aa9268f649fc6757e7b68b180549"};
 	
 	@Override
 	public String[] getLibraries() {
