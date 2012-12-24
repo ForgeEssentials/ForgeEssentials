c70cb0e32306376ab154124ead14726dd5754f50
Lucas Tan <luacs1998@live.com>
2012-12-24 19:08:20 +0800

    Dries, there really is no need for example configs. They can be shipped with the mod, you know.

--- src/FE_SRC_COMMON/com/ForgeEssentials/WorldBorder/ConfigWorldBorder.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/WorldBorder/ConfigWorldBorder.java
@@ -12,7 +12,7 @@ import com.ForgeEssentials.core.ForgeEssentials;
 import com.ForgeEssentials.util.OutputHandler;
 
 /**
- * This generates the configuration structure + an example file.
+ * This generates the configuration structure
  * 
  * @author Dries007
  *
@@ -21,9 +21,8 @@ import com.ForgeEssentials.util.OutputHandler;
 public class ConfigWorldBorder 
 {
 	public static final File wbconfig = new File(ForgeEssentials.FEDIR, "WorldBorder.cfg");
-	public static final File wbconfigExample = new File(ForgeEssentials.FEDIR, "WorldBorder_Example.cfg");
+	
 	public final Configuration config;
-	public static Configuration configExample;
 	
 	/**
 	 * This list makes sure the effect is in the example file.
@@ -33,10 +32,6 @@ public class ConfigWorldBorder
 	
 	public ConfigWorldBorder()
 	{
-		if(wbconfigExample.exists())
-			wbconfigExample.delete();
-		makeExamples();
-		
 		config = new Configuration(wbconfig, true);
 		
 		penaltiesConfig(config);
@@ -114,89 +109,5 @@ public class ConfigWorldBorder
 		}
 	}
 	
-	/*
-	 * Example File
-	 */
-	
-	public static void makeExamples()
-	{
-		configExample = new Configuration(wbconfigExample, true);
-		String penaltyBasePackage = IEffect.class.getPackage().getName() + ".";
-		
-		/*
-		 * PENALTIES
-		 */
-		
-		configExample.addCustomCategoryComment("Penalties", "This is just an example file!");
-		String[] stages = {"Stage1", "Stage2"};
-		stages = configExample.get("Penalties", "stages", stages, "If you add an item here, a subcategory will be generated").valueList;
-		
-		makeExamplesStage1();
-		makeExamplesStage2();
-		
-		configExample.addCustomCategoryComment("Effects", "List of all possible effects you can have in a stage.");
-		
-		for(String effect : PossibleEffects)
-		{
-			try
-			{
-				Class c = Class.forName(penaltyBasePackage + effect.toLowerCase());
-				try 
-				{
-					IEffect pentalty = (IEffect) c.newInstance();
-					pentalty.registerConfig(configExample, "Effects." + effect);
-				}
-				catch (Exception e) 
-				{
-					OutputHandler.SOP("Could not initialize '" + effect + "' in stage '" + "EXAMPLE_FILE" + "'");
-				}
-			}
-			catch (ClassNotFoundException e)
-			{
-				OutputHandler.SOP("'" + effect + "' in the stage '" + "EXAMPLE_FILE" + "' does not exist!");
-			}
-		}
-		
-		/*
-		 * COMMON SETTINGS
-		 */
-		
-		commonConfig(configExample);
-		
-		
-		configExample.save();
-	}
-	
-	public static void makeExamplesStage1()
-	{
-		String penaltyBasePackage = IEffect.class.getPackage().getName() + ".";
-		String stage = "Stage1";
-		String cat = "Penalties." + stage;
-		
-		int dist = configExample.get(cat, "Distance", 0, "The distance outside the border when this gets activated. WARING this needs to be unique! You can specify 2 penalties in 1 stage.").getInt();
-		
-		String[] kinds = {"message", "potion"};
-		kinds = configExample.get(cat, "kind", kinds, "Get the list of possibilitys on the github wiki.").valueList;
-		
-		String[] potionEffects = {"9:5:0"};
-		configExample.get(cat + "." + "potion", "potionEffects", potionEffects, "Format like this: 'ID:duration:amplifire'");
-		configExample.get(cat + "." + "message", "Message", "YOU SHALL NOT PASS!", "Message to send to the player. You can use color codes.");
-	}
-	
-	public static void makeExamplesStage2()
-	{
-		String penaltyBasePackage = IEffect.class.getPackage().getName() + ".";
-		String stage = "Stage2";
-		String cat = "Penalties." + stage;
-		
-		int dist = configExample.get(cat, "Distance", 10, "The distance outside the border when this gets activated. WARING this needs to be unique! You can specify 2 penalties in 1 stage.").getInt();
-		
-		String[] kinds = {"message", "knockback", "damage"};
-		kinds = configExample.get(cat, "kind", kinds, "Get the list of possibilitys on the github wiki.").valueList;
-		
-		configExample.addCustomCategoryComment(cat + ".knockback", "This effect has no option.");
-		configExample.get(cat + "." + "message", "Message", "Told ya!", "Message to send to the player. You can use color codes.");
-		configExample.get(cat + "." + "damage", "damage", 2, "Amount of damage in 1/2 harts.");
-	}
 
 }
