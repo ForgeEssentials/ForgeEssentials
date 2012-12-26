dd51ea8f6f1656d091943d4b051c85e7c5d2afe8
dries kennes <admin@dries007.net>
2012-12-26 17:08:15 +0100

    Build number now gets put in version

--- build.sh
+++ build.sh
@@ -34,5 +34,7 @@ cp -rf ${WORKSPACE}/A1-zipStuff/* .
 cp -rf ${WORKSPACE}/src/FE_SRC_COMMON/com/ForgeEssentials/util/lang/* ./com/ForgeEssentials/util/lang/
 rm ./com/ForgeEssentials/util/lang/dummyForGithub
 
+sed -i 's/@build@/'${BUILD_NUMBER}'/g' mcmod.info
+
 jar cvfm "${WORKSPACE}/${JOB_NAME}-core-${VERSION}.jar" ./META-INF/MANIFEST.MF ./com/ForgeEssentials/core/* ./com/ForgeEssentials/coremod/* ./com/ForgeEssentials/permission/* ./com/ForgeEssentials/util/* ./com/ForgeEssentials/data/* ./com/ForgeEssentials/client/core/* logo.png mcmod.info
 zip -r9 "${WORKSPACE}/${JOB_NAME}-modules-${VERSION}.zip" ./com/ForgeEssentials/client/CUI/* ./com/ForgeEssentials/chat/* ./com/ForgeEssentials/commands/* ./com/ForgeEssentials/economy/* ./com/ForgeEssentials/playerLogger/* ./com/ForgeEssentials/property/* ./com/ForgeEssentials/protection/* ./com/ForgeEssentials/WorldBorder/* ./com/ForgeEssentials/WorldControl/* 
