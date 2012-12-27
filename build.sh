66d449e8d31a2ce5ac79250c1e6ff4b4627c8864
AbrarSyed <sacabrarsyed@gmail.com>
2012-12-27 12:31:46 -0600

    added backup module to build-script

--- build.sh
+++ build.sh
@@ -40,6 +40,6 @@ sed -i 's/@build@/'${BUILD_NUMBER}'/g' mcmod.info
 
 echo "Creating distribution packages"
 jar cvfm "${WORKSPACE}/${JOB_NAME}-core-${MC}-${VERSION}.jar" ./META-INF/MANIFEST.MF ./com/ForgeEssentials/core/* ./com/ForgeEssentials/coremod/* ./com/ForgeEssentials/permission/* ./com/ForgeEssentials/util/* ./com/ForgeEssentials/data/* ./com/ForgeEssentials/client/core/* logo.png mcmod.info
-zip -r9 "${WORKSPACE}/${JOB_NAME}-modules-${MC}-${VERSION}.zip" ./com/ForgeEssentials/client/CUI/* ./com/ForgeEssentials/chat/* ./com/ForgeEssentials/commands/* ./com/ForgeEssentials/economy/* ./com/ForgeEssentials/playerLogger/* ./com/ForgeEssentials/protection/* ./com/ForgeEssentials/WorldBorder/* ./com/ForgeEssentials/WorldControl/* 
+zip -r9 "${WORKSPACE}/${JOB_NAME}-modules-${MC}-${VERSION}.zip" ./com/ForgeEssentials/client/CUI/* ./com/ForgeEssentials/chat/* ./com/ForgeEssentials/commands/* ./com/ForgeEssentials/economy/* ./com/ForgeEssentials/playerLogger/* ./com/ForgeEssentials/protection/* ./com/ForgeEssentials/WorldBorder/* ./com/ForgeEssentials/WorldControl/* ./com/ForgeEssentials/backup/* 
 
 #upload
