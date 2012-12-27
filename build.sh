7b11019d43169c8cb1ea3c158db1d9986c511c67
AbrarSyed <sacabrarsyed@gmail.com>
2012-12-27 10:13:58 -0600

    another buold-script tweak

--- build.sh
+++ build.sh
@@ -11,13 +11,13 @@ echo "Installing Forge..."
 bash ./install.sh
 cd mcp
 
-echo "Copying ${JOB_NAME} into MCP... DOUBLE!!!"
+echo "Copying ${JOB_NAME} into MCP..."
 cd src
 cp -rf ${WORKSPACE}/src/FE_SRC_COMMON/* ./minecraft/
 cp -rf ${WORKSPACE}/src/FE_SRC_CLIENT/* ./minecraft/
 cd ..
 
-echo "adding in libraries..."
+echo "Adding in libraries..."
 cd lib
 cp -rf ${WORKSPACE}/lib/* .
 cd ..
@@ -28,14 +28,16 @@ bash ./recompile.sh
 echo "Reobfuscating..."
 bash ./reobfuscate.sh
 
-echo "Creating server package..."
+echo "Copying in extra files"
 
 cd reobf/minecraft
 cp -rf ${WORKSPACE}/A1-zipStuff/* .
 cp -rf ${WORKSPACE}/src/FE_SRC_COMMON/com/ForgeEssentials/util/lang/* ./com/ForgeEssentials/util/lang/
 rm ./com/ForgeEssentials/util/lang/dummyForGithub
 
+echo "injecting version into mcmod.info"
 sed -i 's/@build@/'${BUILD_NUMBER}'/g' mcmod.info
 
+echo "Creating distribution packages"
 jar cvfm "${WORKSPACE}/${JOB_NAME}-core-${MC}-${VERSION}.jar" ./META-INF/MANIFEST.MF ./com/ForgeEssentials/core/* ./com/ForgeEssentials/coremod/* ./com/ForgeEssentials/permission/* ./com/ForgeEssentials/util/* ./com/ForgeEssentials/data/* ./com/ForgeEssentials/client/core/* logo.png mcmod.info
 zip -r9 "${WORKSPACE}/${JOB_NAME}-modules-${VERSION}.zip" ./com/ForgeEssentials/client/CUI/* ./com/ForgeEssentials/chat/* ./com/ForgeEssentials/commands/* ./com/ForgeEssentials/economy/* ./com/ForgeEssentials/playerLogger/* ./com/ForgeEssentials/property/* ./com/ForgeEssentials/protection/* ./com/ForgeEssentials/WorldBorder/* ./com/ForgeEssentials/WorldControl/* 
