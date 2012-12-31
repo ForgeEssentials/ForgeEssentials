f8c75979b9b13688df2e974a1b800d1f69f5a21c
Lucas Tan <luacs1998@live.com>
2012-12-31 18:27:19 +0800

    Okay, made the thing work.

--- src/FE_SRC_CLIENT/com/ForgeEssentials/client/ForgeEssentialsClient.java
+++ src/FE_SRC_CLIENT/com/ForgeEssentials/client/ForgeEssentialsClient.java
@@ -10,7 +10,8 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler;
 import cpw.mods.fml.common.network.NetworkRegistry;
-
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 @NetworkMod(clientSideRequired = false, serverSideRequired = false, clientPacketHandlerSpec = @SidedPacketHandler(channels = { "ForgeEssentials" }, packetHandler = com.ForgeEssentials.client.network.PacketHandler.class))
 @Mod(modid = "ForgeEssentialsClient", name = "FE|ClientAddon", version = "@VERSION@")
@@ -25,6 +26,7 @@ public class ForgeEssentialsClient
 		assert FMLCommonHandler.instance().getSide().isClient() : new RuntimeException("ForgeEssentialsClient should not be installed on a server!");
 	}
 	
+	@SideOnly(Side.CLIENT)
 	@Init
 	public void load(FMLInitializationEvent e)
 	{
@@ -32,6 +34,7 @@ public class ForgeEssentialsClient
 		MinecraftForge.EVENT_BUS.register(new CUIRenderrer());
 	}
 
+	@SideOnly(Side.CLIENT)
 	public static PlayerInfoClient getInfo()
 	{
 		if (info == null)
@@ -39,6 +42,7 @@ public class ForgeEssentialsClient
 		return info;
 	}
 
+	@SideOnly(Side.CLIENT)
 	public static void setInfo(PlayerInfoClient info)
 	{
 		ForgeEssentialsClient.info = info;
