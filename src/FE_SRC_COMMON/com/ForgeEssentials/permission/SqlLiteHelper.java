a748202732c6f90a672d163a86c0c225410ea946
AbrarSyed <sacabrarsyed@gmail.com>
2013-01-01 10:41:50 -0600

    completed table creation statements

--- src/FE_SRC_COMMON/com/ForgeEssentials/permission/SqlLiteHelper.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/permission/SqlLiteHelper.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 
 public class SqlLiteHelper
 {
+	// TODO: make configureable.
 	private static File			file							= new File(ModulePermissions.permsFolder, "permissions.db");
 
 	private String				DriverClass						= "org.sqlite.JDBC";
@@ -53,6 +54,17 @@ public class SqlLiteHelper
 	// ladderName table
 	private static final String	COLUMN_LADDER_NAME_LADDERID		= "ladderID";
 	private static final String	COLUMN_LADDER_NAME_NAME			= "name";
+	
+	// player table
+	private static final String	COLUMN_PLAYER_PLAYERID			= "playerID";
+	private static final String	COLUMN_PLAYER_USERNAME			= "username";
+	
+	// permissions table
+	private static final String	COLUMN_PERMISSIONS_TARGET			= "target";
+	private static final String	COLUMN_PERMISSIONS_ISGROUP			= "isGroup";
+	private static final String	COLUMN_PERMISSIONS_PERM				= "perm";
+	private static final String	COLUMN_PERMISSIONS_ALLOWED			= "allowed";
+	private static final String	COLUMN_PERMISSIONS_ZONEID			= "zoneID";
 
 	public SqlLiteHelper()
 	{
@@ -90,38 +102,51 @@ public class SqlLiteHelper
 	{
 		String zoneTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_ZONE).append("(")
 				.append(this.COLUMN_ZONE_ZONEID).append(" INTEGER PRIMARY KEY AUTOINCREMENT, ")
-				.append(this.COLUMN_ZONE_NAME).append(" TEXT, ") 
-				.append(this.COLUMN_ZONE_DIM).append(" INTEGER, ") 
-				.append(this.COLUMN_ZONE_PARENT).append(" INTEGER, ") 
+				.append(this.COLUMN_ZONE_NAME).append(" VARCHAR(40), ") 
+				.append(this.COLUMN_ZONE_DIM).append(" SMALLINT, ") 
+				.append(this.COLUMN_ZONE_PARENT).append(" INT ") 
 				.append(")").toString();
 		
-		String groupTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_GROUP_CONNECTOR).append("(")
+		String groupTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_GROUP).append("(")
 				.append(this.COLUMN_GROUP_GROUPID).append("  INTEGER PRIMARY KEY AUTOINCREMENT, ")
-				.append(this.COLUMN_GROUP_NAME).append(" TEXT, ")
+				.append(this.COLUMN_GROUP_NAME).append(" VARCHAR(40), ")
 				.append(this.COLUMN_GROUP_PARENT).append(" INTEGER, ")
-				.append(this.COLUMN_GROUP_PRIORITY).append(" INTEGER, ")
+				.append(this.COLUMN_GROUP_PRIORITY).append(" SMALLINT, ")
 				.append(this.COLUMN_GROUP_ZONE).append(" INTEGER, ")
-				.append(this.COLUMN_GROUP_PREFIX).append(" INTEGER, ")
-				.append(this.COLUMN_GROUP_SUFFIX).append(" INTEGER, ")
+				.append(this.COLUMN_GROUP_PREFIX).append(" VARCHAR(20), ")
+				.append(this.COLUMN_GROUP_SUFFIX).append(" VARCHAR(20) ")
 				.toString();
 		
-		String groupConnectorTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_GROUP).append("(")
+		String groupConnectorTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_GROUP_CONNECTOR).append("(")
 				.append(this.COLUMN_GROUP_CONNECTOR_GROUPID).append(" INTEGER, ")
 				.append(this.COLUMN_GROUP_CONNECTOR_PLAYERID).append(" INTEGER, ")
-				.append(this.COLUMN_GROUP_CONNECTOR_ZONEID).append(" INTEGER, ")
+				.append(this.COLUMN_GROUP_CONNECTOR_ZONEID).append(" INTEGER ")
 				.append(")").toString();
 		
 		
-		String ladderTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_GROUP_CONNECTOR).append("(")
+		String ladderTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_LADDER).append("(")
 				.append(this.COLUMN_LADDER_LADDERID).append(" INTEGER, ")
 				.append(this.COLUMN_LADDER_GROUPID).append(" INTEGER, ")
 				.append(this.COLUMN_LADDER_ZONEID).append(" INTEGER, ")
-				.append(this.COLUMN_LADDER_RANK).append(" INTEGER, ")
+				.append(this.COLUMN_LADDER_RANK).append(" SMALLINT ")
 				.toString();
 		
-		String ladderNameTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_GROUP).append("(")
+		String ladderNameTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_LADDER_NAME).append("(")
 				.append(this.COLUMN_LADDER_NAME_LADDERID).append(" INTEGER PRIMARY KEY AUTOINCREMENT, ")
-				.append(this.COLUMN_LADDER_NAME_NAME).append(" TEXT, ")
+				.append(this.COLUMN_LADDER_NAME_NAME).append(" TEXT ")
+				.append(")").toString();
+		
+		String playerTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_GROUP).append("(")
+				.append(this.COLUMN_PLAYER_PLAYERID).append(" INTEGER PRIMARY KEY AUTOINCREMENT, ")
+				.append(this.COLUMN_PLAYER_USERNAME).append(" VARCHAR(20) ")
+				.append(")").toString();
+		
+		String permissionTable = (new StringBuilder("CREATE TABLE IF NOT EXISTS ")).append(TABLE_PERMISSION).append("(")
+				.append(this.COLUMN_PERMISSIONS_TARGET).append(" VARCHAR(40), ")
+				.append(this.COLUMN_PERMISSIONS_ISGROUP).append(" TINYINT, ")
+				.append(this.COLUMN_PERMISSIONS_PERM).append(" TEXT, ")
+				.append(this.COLUMN_PERMISSIONS_ALLOWED).append(" TINYINT, ")
+				.append(this.COLUMN_PERMISSIONS_ZONEID).append(" INTEGER ")
 				.append(")").toString();
 	}
 }
