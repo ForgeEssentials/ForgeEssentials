0cd445b766fd1c7192260424859edd3050b6391d
Lucas Tan <luacs1998@live.com>
2013-01-08 16:38:34 +0800

    Since we've gone back to hosting everything on github...

--- src/FE_SRC_COMMON/com/ForgeEssentials/coremod/Downloader.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/coremod/Downloader.java
@@ -1,12 +1,11 @@
-package com.ForgeEssentials.coremod.libget;
+package com.ForgeEssentials.coremod;
 
-import com.ForgeEssentials.coremod.Data;
 
 import cpw.mods.fml.relauncher.ILibrarySet;
 
 //Kindly do not reference any FE classes outside the coremod package in this class.
 
-public class SQLDownloader implements ILibrarySet
+public class Downloader implements ILibrarySet
 {
 
 	@Override
