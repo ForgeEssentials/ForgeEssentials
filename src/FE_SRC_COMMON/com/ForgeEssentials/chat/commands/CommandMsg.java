ed33482a26d3d96fa02b0263d99029cf3efb19a3
Malkierian <malkierian@gmail.com>
2013-01-14 21:44:45 -0700

    Fixed /msg saying you're talking to yourself.
    Removed /tell from the overrides.

--- src/FE_SRC_COMMON/com/ForgeEssentials/chat/commands/CommandMsg.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/chat/commands/CommandMsg.java
@@ -105,9 +105,9 @@ public class CommandMsg extends ForgeEssentialsCommandBase
 				}
 				playerReply.put(sender.getCommandSenderName(), receiver.getCommandSenderName());
 				playerReply.put(receiver.getCommandSenderName(), sender.getCommandSenderName());
-				String senderMessage = FEChatFormatCodes.GOLD + "[ me -> " + FEChatFormatCodes.GREY + sender.getCommandSenderName() + FEChatFormatCodes.GOLD
+				String senderMessage = FEChatFormatCodes.GOLD + "[ me -> " + FEChatFormatCodes.GREY + receiver.getCommandSenderName() + FEChatFormatCodes.GOLD
 						+ "] " + FEChatFormatCodes.WHITE;
-				String receiverMessage = FEChatFormatCodes.GOLD + "[" + FEChatFormatCodes.GREY + sender.getCommandSenderName() + FEChatFormatCodes.GOLD
+				String receiverMessage = FEChatFormatCodes.GOLD + "[" + FEChatFormatCodes.GREY + receiver.getCommandSenderName() + FEChatFormatCodes.GOLD
 						+ " -> me ] " + FEChatFormatCodes.WHITE;
 				for (int i = 1; i < args.length; i++)
 				{
