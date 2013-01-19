b8f0ca1f65225c9f33766a8928b26766065921f7
AbrarSyed <sacabrarsyed@gmail.com>
2013-01-19 11:15:03 -0600

    removed extra IF

--- build.sh
+++ build.sh
@@ -11,16 +11,11 @@ echo "Version of ${JOB_NAME} is: ${VERSION} for MC ${MC}"
 echo "Downloading Forge..."
 wget http://files.minecraftforge.net/minecraftforge/minecraftforge-src-latest.zip 
 # if it didn't downlaod, try the mirror...
-if [ ! -f "minecraftforge-src-*.zip" ]
-then
-    echo "Forge server not found, using mirror"
-    wget http://ken.wingedboot.com/forgemirror/files.minecraftforge.net/minecraftforge/minecraftforge-src-latest.zip
-fi
-
-if [ ! -f "$FILE" ];
-then
-   echo "File $FILE does not exist."
-fi
+#if [ ! -f "minecraftforge-src-*.zip" ]
+#then
+#   echo "Forge server not found, using mirror"
+#    wget http://ken.wingedboot.com/forgemirror/files.minecraftforge.net/minecraftforge/minecraftforge-src-latest.zip
+#fi
 
 unzip minecraftforge-src-*.zip
 rm minecraftforge-src-*.zip
