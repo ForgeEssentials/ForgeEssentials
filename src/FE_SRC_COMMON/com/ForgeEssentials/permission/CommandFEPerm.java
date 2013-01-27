f08e9527f39beb48b02b22970f94afc540a94ef7
RlonRyan <rlonryan@aol.com>
2013-01-26 20:31:32 -0700

    Thar

--- src/FE_SRC_COMMON/com/ForgeEssentials/permission/CommandFEPerm.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/permission/CommandFEPerm.java
@@ -168,6 +168,8 @@ public class CommandFEPerm extends ForgeEssentialsCommandBase
 	        
 		}
 		switch (args.length){
+			case 0:
+				return FMLCommonHandler.instance().getSidedDelegate().getServer().getPossibleCompletions(sender, args[args.length - 1]);
 			case 1:
 				return getListOfStringsMatchingLastWord(args, args2);
 			case 2:
@@ -206,7 +208,7 @@ public class CommandFEPerm extends ForgeEssentialsCommandBase
 				}
 				break;
 		}
-		return FMLCommonHandler.instance().getSidedDelegate().getServer().getPossibleCompletions(sender, args[args.length - 1]);
+		return null;
 	}
 	
 }
