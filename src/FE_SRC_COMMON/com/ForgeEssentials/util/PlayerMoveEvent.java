86617efa6c199aa62ad508d34f8a1a33fc7aff4d
AbrarSyed <sacabrarsyed@gmail.com>
2013-01-31 16:25:03 -0600

    started PlayerMove event

---
+++ src/FE_SRC_COMMON/com/ForgeEssentials/util/PlayerMoveEvent.java
@@ -0,0 +1,38 @@
+package com.ForgeEssentials.util;
+
+import com.ForgeEssentials.util.AreaSelector.WarpPoint;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.Cancelable;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+@Cancelable
+public class PlayerMoveEvent extends PlayerEvent
+{
+	public final WarpPoint before;
+	public final WarpPoint after;
+
+	public PlayerMoveEvent(EntityPlayer player, WarpPoint before, WarpPoint after)
+	{
+		super(player);
+		this.before = before;
+		this.after = after;
+	}
+	
+	public boolean isViewMove()
+	{
+		return before.yaw != after.yaw && before.pitch != after.pitch;
+	}
+	
+	public boolean isCoordMove()
+	{
+		return before.xd != after.xd && before.yd != after.yd && before.zd != after.zd; 
+	}
+	
+	public boolean isBlockMove()
+	{
+		return before.x != after.x && before.y != after.y && before.z != after.z; 
+	}
+	
+}
