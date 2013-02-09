86673b6e3423d9f8f9c6547bffc5f885c77610a8
Dries007 <admin@dries007.net>
2013-02-09 01:29:39 +0100

    VIP slots. Fix #95

--- src/FE_SRC_COMMON/com/ForgeEssentials/core/PlayerTracker.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/core/PlayerTracker.java
@@ -1,19 +1,51 @@
 package com.ForgeEssentials.core;
 
+import java.util.Random;
+
+import com.ForgeEssentials.api.permissions.PermissionsAPI;
+import com.ForgeEssentials.api.permissions.query.PermQueryPlayer;
 import com.ForgeEssentials.core.misc.LoginMessage;
+import com.ForgeEssentials.util.FunctionHelper;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.IPlayerTracker;
 
 public class PlayerTracker implements IPlayerTracker
 {
+	public static boolean KickForVIP;
+	public static int VIPslots;
+	public static  String kickMessage;
+	public static final String PERMISSION = "ForgeEssentials.vip";
+
 	@Override
 	public void onPlayerLogin(EntityPlayer player)
 	{
 		PlayerInfo.getPlayerInfo(player.username);
 		LoginMessage.sendLoginMessage(player);
-		//OutputHandler.chatConfirmation(player, "Forge Essentials is still in alpha. There are plenty of incomplete features in the mod. We hope to seek your understanding.");
+		
+		if(KickForVIP)
+		{
+			if(FMLCommonHandler.instance().getMinecraftServerInstance().getAllUsernames().length + VIPslots > FMLCommonHandler.instance().getMinecraftServerInstance().getMaxPlayers())
+			{
+				if(!PermissionsAPI.checkPermAllowed(new PermQueryPlayer(player, PERMISSION)))
+				{
+					((EntityPlayerMP)player).playerNetServerHandler.kickPlayerFromServer(kickMessage);
+				}
+			}
+			
+			while(FMLCommonHandler.instance().getMinecraftServerInstance().getAllUsernames().length + VIPslots > FMLCommonHandler.instance().getMinecraftServerInstance().getMaxPlayers())
+			{
+				EntityPlayerMP player2 = FunctionHelper.getPlayerFromPartialName(FMLCommonHandler.instance().getMinecraftServerInstance().getAllUsernames()[new Random().nextInt(FMLCommonHandler.instance().getMinecraftServerInstance().getAllUsernames().length)]);
+				
+				if(!PermissionsAPI.checkPermAllowed(new PermQueryPlayer(player2, PERMISSION)))
+				{
+					player2.playerNetServerHandler.kickPlayerFromServer(kickMessage);
+				}
+			}
+		}
 	}
 
 	@Override
