cb415e311c90a6b7369d4ff954c627c79d09aaec
Lucas Tan <luacs1998@live.com>
2013-02-17 13:04:29 +0800

    We don't use WorldEdit anymore.

--- src/FE_SRC_COMMON/com/ForgeEssentials/coremod/Data.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/coremod/Data.java
@@ -10,9 +10,9 @@ public class Data
 {
 
 	protected static String[]	libraries		=
-												{ "mysql-connector-java-bin.jar", "H2DB.jar", "worldedit-5.4.6-SNAPSHOT.jar", "metrics-R6-FEmod.jar"};
+												{ "mysql-connector-java-bin.jar", "H2DB.jar", "metrics-R6-FEmod.jar"};
 	protected static String[]	checksums		=
-												{ "3ae0cff91d7f40d5b4c7cefbbd1eab34025bdc15", "32f12e53b4dab80b721525c01d766b95d22129bb", "de96549f9c31aa9268f649fc6757e7b68b180549", "64ffd171d861f38f57534c6180b373b9119a6a2a"};
+												{ "3ae0cff91d7f40d5b4c7cefbbd1eab34025bdc15", "32f12e53b4dab80b721525c01d766b95d22129bb", "64ffd171d861f38f57534c6180b373b9119a6a2a"};
 	protected static String[]	transformers	=
 												{ "com.ForgeEssentials.coremod.transformers.FEPermissionsTransformer", "com.ForgeEssentials.coremod.transformers.FEAccessTransformer", "com.ForgeEssentials.coremod.transformers.FEeventAdder" };
 }
