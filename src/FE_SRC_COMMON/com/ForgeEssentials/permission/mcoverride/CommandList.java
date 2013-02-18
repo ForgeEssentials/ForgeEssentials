678dff0eb00cdd81d783ff90059616eeb0aab6ae
Malkierian <malkierian@gmail.com>
2013-02-18 12:13:29 -0700

    Added override for /list, and added perm node for it to guests by default.

---
+++ src/FE_SRC_COMMON/com/ForgeEssentials/permission/mcoverride/CommandList.java
@@ -0,0 +1,30 @@
+package com.ForgeEssentials.permission.mcoverride;
+
+import net.minecraft.command.CommandServerList;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+
+import com.ForgeEssentials.api.permissions.PermissionsAPI;
+import com.ForgeEssentials.api.permissions.query.PermQueryPlayer;
+
+public class CommandList extends CommandServerList
+{
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender sender)
+	{
+		if (sender instanceof EntityPlayer)
+		{
+			return PermissionsAPI.checkPermAllowed(new PermQueryPlayer((EntityPlayer) sender, getCommandPerm()));
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	public String getCommandPerm()
+	{
+		return "ForgeEssentials.BasicCommands." + getCommandName();
+	}
+}
