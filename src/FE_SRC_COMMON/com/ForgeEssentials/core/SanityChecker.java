651a84383cde13dcd0e356c702eab90626f43949
Lucas Tan <luacs1998@live.com>
2013-02-20 20:06:32 +0800

    Sanity check is now compulsory, will only crash with fihgu mods now.

--- src/FE_SRC_COMMON/com/ForgeEssentials/core/SanityChecker.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/core/SanityChecker.java
@@ -7,18 +7,9 @@ import cpw.mods.fml.common.Loader;
 public class SanityChecker
 {
 	public void run(){
-		
-		
-		
-		if (ForgeEssentials.bukkitcheck = false){
-			OutputHandler.severe("Sanity checking has been disabled. Do not bug the FE team with issues running FE on a bukkit server, or with any other server mod.");
-		}
-		
-		else{
 		// Check for BukkitForge
 		if (Loader.isModLoaded("BukkitForge")){
-			OutputHandler.severe("Sanity check failed: Detected BukkitForge, stopping server for your safety.");
-			throw new RuntimeException("ForgeEssentials: Please do not use FE with BukkitForge, bad things may happen to your server. You were warned.");
+			OutputHandler.severe("Sanity check failed: Detected BukkitForge, bad things may happen, proceed at your own risk.");
 		}
 		
 		// Check for Fihgu's mods
@@ -31,8 +22,7 @@ public class SanityChecker
 			// Check for MCPC+ or LavaBukkit
 			try{
 				Class.forName("org.bukkit.craftbukkit.Main", false, getClass().getClassLoader());
-				OutputHandler.severe("Sanity check failed: Detected a ForgeBukkit server implementation, stopping server for your safety.");
-				throw new RuntimeException("ForgeEssentials: Please do not use FE with any ForgeBukkit server implementation, bad things may happen to your server. You were warned.");
+				OutputHandler.severe("Sanity check failed: Detected a ForgeBukkit server implementation, bad things may happen, proceed at your own risk.");
 			}catch (ClassNotFoundException e){
 			// Safe!
 			}
