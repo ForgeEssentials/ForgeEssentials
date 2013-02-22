2f1c8d9446289d7093b1f94e4f8dd8b513c36a24
Max Bruce <maximumtech11@gmail.com>
2013-02-22 12:31:19 -0800

    Fixed undo & redo bug, still WIP

--- src/FE_SRC_COMMON/com/ForgeEssentials/WorldControl/TickTasks/TickTaskLoadBlockArray.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/WorldControl/TickTasks/TickTaskLoadBlockArray.java
@@ -12,89 +12,40 @@ import com.ForgeEssentials.util.BackupArea;
 import com.ForgeEssentials.util.BlockSaveable;
 import com.ForgeEssentials.util.ITickTask;
 import com.ForgeEssentials.util.OutputHandler;
+import com.ForgeEssentials.util.AreaSelector.AreaBase;
+import com.ForgeEssentials.util.AreaSelector.Point;
 
-public class TickTaskLoadBlockArray implements ITickTask
+public class TickTaskLoadBlockArray extends TickTaskLoadBlocks
 {
-	// stuff needed
-	private final EntityPlayer			player;
 
-	// actually used
-	private final int					last;
-	private int							current;
-	private int							changed;
 	private BlockArray back;
 	private ArrayList<BlockArray.LoadingBlock> blocksToLoad;
 
-	/**
-	 * 
-	 * @param player
-	 * @param back
-	 * BackupArea
-	 * @param before
-	 * true = redo -- false = undo
-	 */
 	public TickTaskLoadBlockArray(EntityPlayer player, BlockArray back)
 	{
-		this.player = player;
+		super(player, new AreaBase(new Point(back.offX, back.offY, back.offZ), new Point(back.offX + back.sizeX, back.offY + back.sizeY, back.offZ + back.sizeZ)));
 		this.back = back;
 		blocksToLoad = this.back.getBlocksToLoad();
-		last = back.sizeX * back.sizeY * back.sizeZ;
+		last = blocksToLoad.size();
+	}
+	
+	protected boolean usesCoordsSystem() {
+		return back.isRelative;
+	}
+	
+	protected boolean requiresBackup() {
+		return back.isRelative;
 	}
 
-	@Override
-	public void tick()
-	{
-		int lastChanged = changed;
-		
-		int xzBlocksSquared = back.sizeX * back.sizeZ;
-		
-		int x = 0;
-		int z = 0;
-		int y = 0;
-		
-
-		for (int i = current; i <= last; i++)
-		{
-			current = i;
-
-			if (blocksToLoad.get(i).placeBlock(player.worldObj, (back.isRelative?MathHelper.floor_double(player.posX):0) + back.offX + x, (back.isRelative?MathHelper.floor_double(player.posY):0) + back.offY + y, (back.isRelative?MathHelper.floor_double(player.posZ):0) + back.offZ + z))
-			{
-				changed++;
-			}
-
-			if (lastChanged >= ConfigWorldControl.blocksPerTick)
-			{
-				return;
-			}
-			x++;
-			if(x>back.sizeX) {
-				x = 0;
-				z++;
-			}
-			if(z>back.sizeZ) {
-				z = 0;
-				y++;
-			}
-			if(y>back.sizeY)current = last;
+	protected boolean placeBlock() {
+		if(back.isRelative) {
+			
+		}else{
+			BlockArray.LoadingBlock block = blocksToLoad.get(current);
+			System.out.println(block.x+", "+block.z);
+			return place(block.x, block.y, block.z, block);
 		}
+		return false;
 	}
-
-	@Override
-	public void onComplete()
-	{
-		OutputHandler.chatConfirmation(player, "" + changed + " blocks changed");
-	}
-
-	@Override
-	public boolean isComplete()
-	{
-		return current == last;
-	}
-
-	@Override
-	public boolean editsBlocks()
-	{
-		return true;
-	}
-
+	
 }
