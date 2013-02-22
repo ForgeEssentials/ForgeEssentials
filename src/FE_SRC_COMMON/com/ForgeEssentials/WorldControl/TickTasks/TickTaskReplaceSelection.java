c3c75a16a68e88190682d7b607b0043f164292f6
Max Bruce <maximumtech11@gmail.com>
2013-02-21 17:30:28 -0800

    Cleaned up code, made operational

--- src/FE_SRC_COMMON/com/ForgeEssentials/WorldControl/TickTasks/TickTaskReplaceSelection.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/WorldControl/TickTasks/TickTaskReplaceSelection.java
@@ -24,14 +24,11 @@ public class TickTaskReplaceSelection extends TickTaskLoadBlocks
 	BlockInfo from;
 	BlockInfo to;
 
-	private Point current;
-
 	public TickTaskReplaceSelection(EntityPlayer player, AreaBase sel, BlockInfo from, BlockInfo to)
 	{
 		super(player, sel);
 		this.from=from;
 		this.to=to;
-		current = sel.getLowPoint();
 	}
 
 	public TickTaskReplaceSelection(EntityPlayer player, AreaBase sel, BlockInfo from, BlockInfo to, ArrayList<AreaBase> applicable)
@@ -40,47 +37,14 @@ public class TickTaskReplaceSelection extends TickTaskLoadBlocks
 		this.applicable = applicable;
 	}
 	
-	protected void runTick() {
-		x = current.x;
-		y = current.y;
-		z = current.z;
-	}
-	
-	private int x;
-	private int y;
-	private int z;
-	
 	protected boolean placeBlock() {
 		int id = world.getBlockId(x, y, z);
 		int m = world.getBlockMetadata(x, y, z);
-		boolean ret = false;
 		if (from.compare(new BlockInfo.SingularBlockInfo(Block.blocksList[id], m, null)) && isApplicable(x, y, z))
 		{
-			ret = place(x, y, z, to.randomBlock());
+			return place(x, y, z, to.randomBlock());
 		}
-		x++;
-		if (x > sel.getHighPoint().x)
-		{
-			// Reset y, increment z.
-			x = sel.getLowPoint().x;
-			z++;
-			if (z > sel.getHighPoint().z)
-			{
-				// Reset z, increment x.
-				z = sel.getLowPoint().z;
-				y++;
-				// Check stop condition
-				if (y > sel.getHighPoint().y)
-				{
-					return ret;
-				}
-			}
-		}
-		return ret;
-	}
-	
-	protected void endTick() {
-		current = new Point(x, y, z);
+		return false;
 	}
 
 	private boolean isApplicable(int x, int y, int z)
