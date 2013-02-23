216f42c2a5b7bc55f37bb2a250c3b947f71019aa
Max Bruce <maximumtech11@gmail.com>
2013-02-22 18:43:48 -0800

    Functionality = player pos w/ no args

--- src/FE_SRC_COMMON/com/ForgeEssentials/WorldControl/commands/CommandPos.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/WorldControl/commands/CommandPos.java
@@ -66,17 +66,17 @@ public class CommandPos extends WorldControlCommandBase
 			return;
 		}else{
 
-			MovingObjectPosition mop = FunctionHelper.getPlayerLookingSpot(player, false);
+			//MovingObjectPosition mop = FunctionHelper.getPlayerLookingSpot(player, false);
 
-			if (mop == null)
-			{
-				OutputHandler.chatError(player, Localization.get(Localization.ERROR_TARGET));
-				return;
-			}
+			//if (mop == null)
+			//{
+			//	OutputHandler.chatError(player, Localization.get(Localization.ERROR_TARGET));
+			//	return;
+			//}
 
-			x = mop.blockX;
-			y = mop.blockY;
-			z = mop.blockZ;
+			x = (int)player.posX-1;
+			y = (int)player.posY;
+			z = (int)player.posZ;
 
 			Point point = new Point(x, y, z);
 			if (!PermissionsAPI.checkPermAllowed(new PermQueryPlayerArea(player, getCommandPerm(), point)))
