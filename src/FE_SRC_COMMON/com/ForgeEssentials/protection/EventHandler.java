503bdbb07e53e379773286853351dd2e551e3b28
AbrarSyed <sacabrarsyed@gmail.com>
2013-02-27 21:50:01 -0600

    fixed entityInteractions permissions

--- src/FE_SRC_COMMON/com/ForgeEssentials/protection/EventHandler.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/protection/EventHandler.java
@@ -1,8 +1,10 @@
 package com.ForgeEssentials.protection;
 
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.EventPriority;
 import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.EntityInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -27,17 +29,27 @@ public class EventHandler
 
 		if (e.target instanceof EntityPlayer)
 		{
+			// Stops players from hitting each other.
+
 			boolean sourceB = !PermissionsAPI.checkPermAllowed(new PermQueryPlayer(e.entityPlayer, ModuleProtection.PERM_PVP));
-			boolean receiverB = !PermissionsAPI.checkPermAllowed(new PermQueryPlayer(e.entityPlayer, ModuleProtection.PERM_PVP));
+
+			if (sourceB)
+			{
+				e.setCanceled(true);
+				return;
+			}
+
+			boolean receiverB = !PermissionsAPI.checkPermAllowed(new PermQueryPlayer((EntityPlayer) e.target, ModuleProtection.PERM_PVP));
 
 			if (sourceB || receiverB)
 			{
 				e.setCanceled(true);
 			}
+
 		}
 		else
 		{
-			// stopping entity damage
+			// Stops players from hitting entities.
 
 			PermQuery query = new PermQueryPlayer(e.entityPlayer, ModuleProtection.PERM_OVERRIDE);
 			Boolean result = PermissionsAPI.checkPermAllowed(query);
@@ -45,6 +57,73 @@ public class EventHandler
 			if (!result)
 			{
 				query = new PermQueryPlayer(e.entityPlayer, ModuleProtection.PERM_INTERACT_ENTITY);
+				result = PermissionsAPI.checkPermAllowed(query);
+			}
+
+			e.setCanceled(!result);
+		}
+	}
+
+	@ForgeSubscribe(priority = EventPriority.LOW)
+	public void damage(LivingHurtEvent e)
+	{
+		// do nothing if the source isnt a living thing.
+		// actually... if its ANY entity.
+		if (e.source.getEntity() == null || !(e.source.getEntity() instanceof EntityLiving))
+			return;
+
+		EntityLiving source = (EntityLiving) e.source.getEntity();
+		
+		boolean sourcePlayer = e.source.getEntity() instanceof EntityPlayer;
+		boolean targetPlayer = e.entityLiving instanceof EntityPlayer;
+
+		if (e.entityLiving == null)
+			return;
+
+		if (sourcePlayer && targetPlayer)
+		{
+			
+			// PVP checks
+			
+			boolean sourceB = !PermissionsAPI.checkPermAllowed(new PermQueryPlayerArea((EntityPlayer) e.entityLiving, ModuleProtection.PERM_PVP, new WorldPoint(e.source.getEntity())));
+			
+			if (sourceB)
+			{
+				e.setCanceled(true);
+				return;
+			}
+			
+			boolean receiverB = !PermissionsAPI.checkPermAllowed(new PermQueryPlayer((EntityPlayer) e.source.getEntity(), ModuleProtection.PERM_PVP));
+
+			
+			if (sourceB || receiverB)
+				e.setCanceled(true);
+		}
+		else if (sourcePlayer)
+		{
+				// stop players hitting animals.
+
+				PermQuery query = new PermQueryPlayerArea((EntityPlayer) source, ModuleProtection.PERM_OVERRIDE, new WorldPoint(e.entityLiving));
+				Boolean result = PermissionsAPI.checkPermAllowed(query);
+
+				if (!result)
+				{
+					query = new PermQueryPlayerArea((EntityPlayer) source, ModuleProtection.PERM_INTERACT_ENTITY, new WorldPoint(e.entityLiving));
+					result = PermissionsAPI.checkPermAllowed(query);
+				}
+
+				e.setCanceled(!result);
+		}
+		else if (targetPlayer)
+		{
+			// stop people from hitting entites.
+
+			PermQuery query = new PermQueryPlayer((EntityPlayer) e.entityLiving, ModuleProtection.PERM_OVERRIDE);
+			Boolean result = PermissionsAPI.checkPermAllowed(query);
+
+			if (!result)
+			{
+				query = new PermQueryPlayer((EntityPlayer) e.entityLiving, ModuleProtection.PERM_INTERACT_ENTITY);
 				result = PermissionsAPI.checkPermAllowed(query);
 			}
 
