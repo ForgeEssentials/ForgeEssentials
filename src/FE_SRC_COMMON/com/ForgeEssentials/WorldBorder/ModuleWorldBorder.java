3d475d4d34550096e3523da1dce209b166770176
AbrarSyed <sacabrarsyed@gmail.com>
2013-03-21 01:18:40 -0500

    stopped insane ammounts of lag

--- src/FE_SRC_COMMON/com/ForgeEssentials/WorldBorder/ModuleWorldBorder.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/WorldBorder/ModuleWorldBorder.java
@@ -13,6 +13,7 @@ import com.ForgeEssentials.api.ForgeEssentialsRegistrar.PermRegister;
 import com.ForgeEssentials.api.data.ClassContainer;
 import com.ForgeEssentials.api.data.DataStorageManager;
 import com.ForgeEssentials.api.modules.FEModule;
+import com.ForgeEssentials.api.modules.event.FEModuleInitEvent;
 import com.ForgeEssentials.api.modules.event.FEModuleServerInitEvent;
 import com.ForgeEssentials.api.modules.event.FEModuleServerStopEvent;
 import com.ForgeEssentials.api.permissions.IPermRegisterEvent;
@@ -24,6 +25,8 @@ import com.ForgeEssentials.util.OutputHandler;
 import com.ForgeEssentials.util.AreaSelector.Point;
 import com.ForgeEssentials.util.events.PlayerMoveEvent;
 import com.ForgeEssentials.util.vector.Vector2;
+
+import cpw.mods.fml.common.FMLCommonHandler;
 
 /**
  * Bounces players back into the border if they pass it. No bypass permissions
@@ -38,14 +41,14 @@ public class ModuleWorldBorder
 
 	@FEModule.Config
 	public static ConfigWorldBorder				config;
-	
-	public static HashMap<String, WorldBorder>	borderMap 		= new HashMap<String, WorldBorder>();		
+
+	public static HashMap<String, WorldBorder>	borderMap		= new HashMap<String, WorldBorder>();
 
 	public static HashMap<Integer, IEffect[]>	effectsList		= new HashMap<Integer, IEffect[]>();
 	public static int							overGenerate	= 345;
-	
+
 	public static boolean						globalOverride	= true;
-	static final ClassContainer					con	= new ClassContainer(WorldBorder.class);
+	static final ClassContainer					con				= new ClassContainer(WorldBorder.class);
 
 	public ModuleWorldBorder()
 	{
@@ -58,13 +61,18 @@ public class ModuleWorldBorder
 		event.registerPermissionLevel("ForgeEssentials.WorldBorder.admin", RegGroup.OWNERS);
 	}
 
+	@FEModule.Init
+	public void load(FEModuleInitEvent e)
+	{
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
 	@FEModule.ServerInit
 	public void serverStarting(FEModuleServerInitEvent e)
 	{
 		e.registerServerCommand(new CommandWB());
-		MinecraftForge.EVENT_BUS.register(this);
 		loadAll();
-		
+
 		Zone zone = ZoneManager.getGLOBAL();
 		if (!borderMap.containsKey(zone.getZoneName()))
 		{
@@ -77,23 +85,28 @@ public class ModuleWorldBorder
 	{
 		saveAll();
 	}
-	
+
 	@ForgeSubscribe
 	public void playerMove(PlayerMoveEvent e)
 	{
 		if (WBenabled)
 		{
-			borderMap.get(ZoneManager.getWorldZone(e.entityPlayer.worldObj).getZoneName()).check((EntityPlayerMP) e.entityPlayer);
+			Zone zone = ZoneManager.getWorldZone(e.entityPlayer.worldObj);
+			WorldBorder border = borderMap.get(zone.getZoneName());
+			border.check((EntityPlayerMP) e.entityPlayer);
 			if (globalOverride)
 			{
 				borderMap.get(ZoneManager.getGLOBAL().getZoneName()).check((EntityPlayerMP) e.entityPlayer);
 			}
 		}
 	}
-	
+
 	@ForgeSubscribe
-	public void WorldEvent(WorldEvent e)
+	public void worldLoad(WorldEvent.Load e)
 	{
+		if (FMLCommonHandler.instance().getEffectiveSide().isClient())
+			return;
+
 		Zone zone = ZoneManager.getWorldZone(e.world);
 		if (borderMap.containsKey(zone.getZoneName()))
 		{
@@ -108,13 +121,18 @@ public class ModuleWorldBorder
 				borderMap.put(zone.getZoneName(), new WorldBorder(zone));
 			DataStorageManager.getReccomendedDriver().saveObject(con, borderMap.get(zone.getZoneName()));
 		}
-		
-		if (e instanceof WorldEvent.Unload)
-		{
-			borderMap.remove(zone.getZoneName());
-		}
 	}
-	
+
+	@ForgeSubscribe
+	public void worldUnLoad(WorldEvent.Unload e)
+	{
+		if (FMLCommonHandler.instance().getEffectiveSide().isClient())
+			return;
+
+		Zone zone = ZoneManager.getWorldZone(e.world);
+		borderMap.remove(zone.getZoneName());
+	}
+
 	public static void loadAll()
 	{
 		for (Object obj : DataStorageManager.getReccomendedDriver().loadAllObjects(con))
@@ -131,7 +149,7 @@ public class ModuleWorldBorder
 			wb.save();
 		}
 	}
-	
+
 	/*
 	 * Penalty part
 	 */
@@ -176,7 +194,7 @@ public class ModuleWorldBorder
 	{
 		double difX = center.x - player.posX;
 		double difZ = center.z - player.posZ;
-		
+
 		return (int) Math.sqrt(Math.pow(difX, 2) + Math.pow(difZ, 2));
 	}
 
@@ -184,7 +202,7 @@ public class ModuleWorldBorder
 	{
 		double difX = centerX - x;
 		double difZ = centerZ - z;
-		
+
 		return (int) Math.sqrt(Math.pow(difX, 2) + Math.pow(difZ, 2));
 	}
 }
