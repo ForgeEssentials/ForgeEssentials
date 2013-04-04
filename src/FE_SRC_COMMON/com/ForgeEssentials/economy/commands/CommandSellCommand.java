1f4184df2b3db1718c63e7fb991cdbf951f5bb1b
jgdovin <joshgdovin@gmail.com>
2013-04-03 21:49:05 -0400

    Fixed Economy again... please dont break!

--- src/FE_SRC_COMMON/com/ForgeEssentials/economy/commands/CommandSellCommand.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/economy/commands/CommandSellCommand.java
@@ -11,6 +11,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 
 import com.ForgeEssentials.core.commands.ForgeEssentialsCommandBase;
+import com.ForgeEssentials.util.FunctionHelper;
 import com.ForgeEssentials.util.Localization;
 import com.ForgeEssentials.util.OutputHandler;
 
@@ -42,74 +43,71 @@ public class CommandSellCommand extends ForgeEssentialsCommandBase
 		System.out.print(sender);
 		if (args.length >= 3)
 		{
-			List<EntityPlayerMP> players = Arrays.asList(PlayerSelector.matchPlayers(sender, args[0]));
-			if (PlayerSelector.hasArguments(args[0]))
-			{
-				players = Arrays.asList(PlayerSelector.matchPlayers(sender, args[0]));
-			}
-			if (players.size() != 0)
-			{
-				for (EntityPlayer player : players)
-				{
-					boolean found = false;
-					// Set needed parm
-					int amount = 1, item = 0, meta = -1;
-					ItemStack target = new ItemStack(item, amount, meta);
 
-					if (args[1].contains("x"))
+			EntityPlayerMP player = FunctionHelper.getPlayerForName(sender, args[0]);
+			if (player != null)
+			{
+
+				boolean found = false;
+				// Set needed parm
+				int amount = 1, item = 0, meta = -1;
+				ItemStack target = new ItemStack(item, amount, meta);
+
+				if (args[1].contains("x"))
+				{
+					String[] split = args[1].split("x");
+					target.stackSize = amount = parseIntBounded(sender, split[0], 0, 64);
+					args[1] = split[1];
+				}
+				if (args[1].contains(":"))
+				{
+					String[] split = args[1].split(":");
+					target.setItemDamage(meta = parseInt(sender, split[1]));
+					args[1] = split[0];
+				}
+				target.itemID = item = parseIntWithMin(sender, args[1], 0);
+				// Loop though inv and find a stack big enough to support the sell cmd
+				for (int slot = 0; slot < player.inventory.mainInventory.length; slot++)
+				{
+					ItemStack is = player.inventory.mainInventory[slot];
+					if (is != null)
 					{
-						String[] split = args[1].split("x");
-						target.stackSize = amount = parseIntBounded(sender, split[0], 0, 64);
-						args[1] = split[1];
-					}
-					if (args[1].contains(":"))
-					{
-						String[] split = args[1].split(":");
-						target.setItemDamage(meta = parseInt(sender, split[1]));
-						args[1] = split[0];
-					}
-					target.itemID = item = parseIntWithMin(sender, args[1], 0);
-					// Loop though inv and find a stack big enough to support the sell cmd
-					for (int slot = 0; slot < player.inventory.mainInventory.length; slot++)
-					{
-						ItemStack is = player.inventory.mainInventory[slot];
-						if (is != null)
+						if (is.itemID == item)
 						{
-							if (is.itemID == item)
+							if (meta == -1 || meta == is.getItemDamage())
 							{
-								if (meta == -1 || meta == is.getItemDamage())
+								if (is.stackSize >= amount)
 								{
-									if (is.stackSize >= amount)
-									{
-										player.inventory.decrStackSize(slot, amount);
-										found = true;
-										break;
-									}
+									player.inventory.decrStackSize(slot, amount);
+									found = true;
+									break;
 								}
 							}
 						}
 					}
-					if (found)
-					{
-						// Do command in name of player
+				}
+				if (found)
+				{
+					// Do command in name of player
 
-						StringBuilder cmd = new StringBuilder(args.toString().length());
-						for (int i = 2; i < args.length; i++)
-						{
-							cmd.append(args[i]);
-							cmd.append(" ");
-						}
-						MinecraftServer.getServer().executeCommand(cmd.toString());
-						OutputHandler.chatConfirmation(player, "That cost you " + amount + " x " + target.getDisplayName());
-					}
-					else
+					StringBuilder cmd = new StringBuilder(args.toString().length());
+					for (int i = 2; i < args.length; i++)
 					{
-						OutputHandler.chatError(player, "You can't afford that!!");
+						cmd.append(args[i]);
+						cmd.append(" ");
 					}
+					MinecraftServer.getServer().executeCommand(cmd.toString());
+					OutputHandler.chatConfirmation(player, "That cost you " + amount + " x " + target.getDisplayName());
+				}
+				else
+				{
+					//this should be removed
+					OutputHandler.chatError(player, "You can't afford that!!");
 				}
 			}
 			else
 			{
+				//this should be removed
 				OutputHandler.chatError(sender, Localization.format(Localization.ERROR_NOPLAYER, args[0]));
 			}
 		}
