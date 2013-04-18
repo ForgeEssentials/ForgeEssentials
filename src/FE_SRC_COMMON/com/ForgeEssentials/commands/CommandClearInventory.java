0a78b704214823638335fb7a0fea2437e99b856d
Allen Cook <alcook16@gmail.com>
2013-04-18 19:25:14 -0400

    Added Commands drop, pulse, push, status, clear
    Command pulse needs to have blockpoweredore replaced with
    blockpoweredoremod found in util and clear uses the
    inventoryplayerextended to handle qty clear

--- src/FE_SRC_COMMON/com/ForgeEssentials/commands/CommandClearInventory.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/commands/CommandClearInventory.java
@@ -33,40 +33,46 @@ public class CommandClearInventory extends FEcmdModuleCommands
 	}
 
 	@Override
-	public void processCommandPlayer(EntityPlayer sender, String[] args)
-	{
-		if (args.length == 0)
-		{
-			sender.inventory.clearInventory(-1, -1);
-			sender.inventoryContainer.detectAndSendChanges();
-			OutputHandler.chatConfirmation(sender, Localization.get("command.clear.doneSelf"));
-		}
-		else if (args.length >= 1 && PermissionsAPI.checkPermAllowed(new PermQueryPlayer(sender, getCommandPerm() + ".others")))
-		{
-			EntityPlayerMP player = FunctionHelper.getPlayerForName(sender, args[0]);
-			if (player != null)
-			{
-				int clearPar1 = -1, clearPar2 = -1;
-				if (args.length >= 2)
-				{
-					clearPar1 = parseInt(sender, args[1]);
-					clearPar2 = parseInt(sender, args[2]);
-				}
-				player.inventory.clearInventory(clearPar1, clearPar2);
-				player.inventoryContainer.detectAndSendChanges();
-				OutputHandler.chatWarning(sender, Localization.format("command.clear.doneBy", sender.getCommandSenderName()));
-				OutputHandler.chatConfirmation(sender, Localization.format("command.clear.doneOf", args[0]));
-			}
-			else
-			{
-				OutputHandler.chatError(sender, Localization.format(Localization.ERROR_NOPLAYER, args[0]));
-			}
-		}
-		else
-		{
-			sender.sendChatToPlayer(Localization.get(Localization.ERROR_BADSYNTAX) + getSyntaxPlayer(sender));
-		}
-	}
+	public void processCommand(ICommandSender par1ICommandSender, String[] par2ArrayOfStr)
+    {
+        EntityPlayerMP var3 = par2ArrayOfStr.length == 0 ? getCommandSenderAsPlayer(par1ICommandSender) : func_82359_c(par1ICommandSender, par2ArrayOfStr[0]);
+        int var4 = par2ArrayOfStr.length >= 2 ? parseIntWithMin(par1ICommandSender, par2ArrayOfStr[1], 1) : -1;
+        int var5 = par2ArrayOfStr.length >= 3 ? parseIntWithMin(par1ICommandSender, par2ArrayOfStr[2], 0) : -1;
+        int var7;
+        ItemStack[] MainHold = null;
+        ItemStack[] ArmorHold = null;
+        if (par2ArrayOfStr.length == 4)
+        {
+        	 MainHold = (ItemStack[])var3.inventory.mainInventory.clone();
+            ArmorHold = (ItemStack[])var3.inventory.armorInventory.clone();
+        	InventoryPlayerExtend test = new InventoryPlayerExtend(var3);
+        	test.armorInventory = var3.inventory.armorInventory;
+        	test.mainInventory = var3.inventory.mainInventory;
+            var7 = test.clearInventory(var4, var5, parseIntWithMin(par1ICommandSender, par2ArrayOfStr[3], 0));
+        }
+        else
+        {
+            var7 = var3.inventory.clearInventory(var4, var5);
+        }
+
+        var3.inventoryContainer.detectAndSendChanges();
+
+        if (var7 == -1)
+        {
+        	var3.inventory.mainInventory = MainHold;
+        	var3.inventory.armorInventory = ArmorHold;
+            throw new CommandException("Not Enough of the given Item + metadata in" + var3.getEntityName() + "\'s inventory", new Object[] {var3.getEntityName()});
+        }
+        else if (var7 == 0)
+        {
+            throw new CommandException("commands.clear.failure", new Object[] {var3.getEntityName()});
+        }
+        else
+        {
+            notifyAdmins(par1ICommandSender, "commands.clear.success", new Object[] {var3.getEntityName(), Integer.valueOf(var7)});
+        }
+    }
+
 
 	@Override
 	public void processCommandConsole(ICommandSender sender, String[] args)
