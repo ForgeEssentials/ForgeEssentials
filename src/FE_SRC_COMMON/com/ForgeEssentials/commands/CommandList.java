bf8649d2e28d362d80d54735fa09d5b321a5d18d
Dries007 <admin@dries007.net>
2013-04-25 19:25:58 +0200

    why y no push

---
+++ src/FE_SRC_COMMON/com/ForgeEssentials/commands/CommandList.java
@@ -0,0 +1,75 @@
+package com.ForgeEssentials.commands;
+
+import java.util.List;
+
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+
+import com.ForgeEssentials.api.permissions.RegGroup;
+import com.ForgeEssentials.commands.util.FEcmdModuleCommands;
+import com.ForgeEssentials.util.FunctionHelper;
+import com.google.common.collect.HashMultimap;
+
+public class CommandList extends FEcmdModuleCommands
+{
+    @Override
+    public String[] getDefaultAliases()
+    {
+        return new String[] {"who", "online", "players"};
+    }
+    
+    @Override
+    public String getCommandName()
+    {
+        return "list";
+    }
+
+    @Override
+    public RegGroup getReggroup()
+    {
+        return RegGroup.GUESTS;
+    }
+
+    @Override
+    public void processCommandPlayer(EntityPlayer sender, String[] args)
+    {
+        sendList(sender);
+    }
+
+    @Override
+    public void processCommandConsole(ICommandSender sender, String[] args)
+    {
+        sendList(sender);
+    }
+
+    private void sendList(ICommandSender sender)
+    {
+        // Group => Player(s)
+        HashMultimap<String, String> map = HashMultimap.create();
+        for (String username : MinecraftServer.getServer().getConfigurationManager().getAllUsernames())
+            map.put(FunctionHelper.getGroupRankString(username), username);
+        
+        sender.sendChatToPlayer(sender.translateString("commands.players.list", new Object[] {Integer.valueOf(MinecraftServer.getServer().getCurrentPlayerCount()), Integer.valueOf(MinecraftServer.getServer().getMaxPlayers())}));
+        for (String group : map.keySet())
+            sender.sendChatToPlayer(group + ": " + FunctionHelper.niceJoin(map.get(group).toArray()));
+    }
+
+    @Override
+    public boolean canConsoleUseCommand()
+    {
+        return true;
+    }
+
+    @Override
+    public List<?> addTabCompletionOptions(ICommandSender sender, String[] args)
+    {
+        return null;
+    }
+
+    @Override
+    public String getCommandPerm()
+    {
+        return "ForgeEssentials.BasicCommands." + getCommandName();
+    }
+}
