24da75e5975404f7d02fb2eca4ff784253b9d041
RlonRyan <rlonryan@aol.com>
2013-06-26 23:33:20 -0600

    Update ModuleScripting.java

--- src/FE_SRC_COMMON/com/ForgeEssentials/scripting/ModuleScripting.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/scripting/ModuleScripting.java
@@ -7,7 +7,9 @@ import com.ForgeEssentials.core.moduleLauncher.FEModule;
 import com.ForgeEssentials.core.moduleLauncher.FEModule.ModuleDir;
 import com.ForgeEssentials.util.OutputHandler;
 import com.ForgeEssentials.util.events.modules.FEModulePreInitEvent;
+import com.ForgeEssentials.util.events.modules.FEModuleServerInitEvent;
 
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @FEModule(name = "scripting", parentMod = ForgeEssentials.class, isCore = false)
@@ -22,19 +24,43 @@ public class ModuleScripting {
 		GameRegistry.registerPlayerTracker(new ScriptPlayerTracker());
 	}
 	
+	@FEModule.ServerInit
+	public void serverStarting(FEModuleServerInitEvent e)
+	{
+		e.registerServerCommand(new CommandScript());
+	}
+	
 	public static void startup(){
 		try{
-			if (!ScriptPlayerTracker.scriptfolder.exists()){
+			if (!ScriptPlayerTracker.scriptfolder.exists())
+			{
 				ScriptPlayerTracker.scriptfolder.mkdirs();
-				
-			}else if (!ScriptPlayerTracker.loginplayer.exists()){
+				// Since it is safe to assume the sub-folders don't exist.
 				ScriptPlayerTracker.loginplayer.mkdirs();
-			}else if (!ScriptPlayerTracker.logingroup.exists()){
 				ScriptPlayerTracker.logingroup.mkdirs();
-			}else if (!ScriptPlayerTracker.respawngroup.exists()){
 				ScriptPlayerTracker.respawngroup.mkdirs();
-			}else if (!ScriptPlayerTracker.respawnplayer.exists()){
 				ScriptPlayerTracker.respawnplayer.mkdirs();
+				CommandScript.commandscripts.mkdirs();
+			}
+			if (!ScriptPlayerTracker.loginplayer.exists())
+			{
+				ScriptPlayerTracker.loginplayer.mkdirs();
+			}
+			if (!ScriptPlayerTracker.logingroup.exists())
+			{
+				ScriptPlayerTracker.logingroup.mkdirs();
+			}
+			if (!ScriptPlayerTracker.respawngroup.exists())
+			{
+				ScriptPlayerTracker.respawngroup.mkdirs();
+			}
+			if (!ScriptPlayerTracker.respawnplayer.exists())
+			{
+				ScriptPlayerTracker.respawnplayer.mkdirs();
+			}
+			if (!CommandScript.commandscripts.exists())
+			{
+				CommandScript.commandscripts.mkdirs();
 			}
 		}catch (Exception e){
 			OutputHandler.felog.warning("Could not setup scripting folders - you might have to do it yourself.");
