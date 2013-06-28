6a2989eec947236a7fc7620d94b1021f298c0334
RlonRyan <rlonryan@aol.com>
2013-06-27 22:55:48 -0600

    Books!

--- src/FE_SRC_COMMON/com/ForgeEssentials/util/FunctionHelper.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/util/FunctionHelper.java
@@ -844,6 +844,80 @@ public final class FunctionHelper
 		player.inventory.addItemStackToInventory(is);
 	}
 
+	public static void getBookFromFile(EntityPlayer player, File file, String title)
+	{
+		NBTTagCompound tag = new NBTTagCompound();
+		NBTTagList pages = new NBTTagList();
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		if (file.isFile())
+		{
+			if (file.getName().contains(".txt"))
+			{
+				List<String> lines = new ArrayList<String>();
+				try
+				{
+					lines.add(FEChatFormatCodes.GREEN + "START" + FEChatFormatCodes.BLACK);
+					lines.add("");
+					FileInputStream stream = new FileInputStream(file);
+					InputStreamReader streamReader = new InputStreamReader(stream);
+					BufferedReader reader = new BufferedReader(streamReader);
+					String line = reader.readLine();
+					while (line != null)
+					{
+						while (line.length() > 21)
+						{
+							lines.add(line.substring(0, 20));
+							line = line.substring(20);
+						}
+						lines.add(line);
+						line = reader.readLine();
+					}
+					reader.close();
+					streamReader.close();
+					stream.close();
+					lines.add("");
+					lines.add(FEChatFormatCodes.RED + "END" + FEChatFormatCodes.BLACK);
+
+				}
+				catch (Exception e)
+				{
+					OutputHandler.felog.warning("Error reading script: " + file.getName());
+				}
+				int part = 0;
+				int parts = lines.size() / 10 + 1;
+				String filename = file.getName().replaceAll(".txt", "");
+				if (filename.length() > 13)
+					filename = filename.substring(0, 10) + "...";
+				while (lines.size() != 0)
+				{
+					part++;
+					String temp = "";
+					for (int i = 0; i < 10 && lines.size() > 0; i++)
+					{
+						temp += lines.get(0) + "\n";
+						lines.remove(0);
+					}
+					map.put(FEChatFormatCodes.GOLD + " File: " + FEChatFormatCodes.GREY + filename + FEChatFormatCodes.DARKGREY + "\nPart " + part + " of " + parts + FEChatFormatCodes.BLACK + "\n\n", temp);
+				}
+			}
+		}
+
+		SortedSet<String> keys = new TreeSet<String>(map.keySet());
+		for (String name : keys)
+		{
+			pages.appendTag(new NBTTagString("", name + map.get(name)));
+		}
+
+		tag.setString("author", "ForgeEssentials");
+		tag.setString("title", title);
+		tag.setTag("pages", pages);
+
+		ItemStack is = new ItemStack(Item.writtenBook);
+		is.setTagCompound(tag);
+		player.inventory.addItemStackToInventory(is);
+	}
+
 	public static void getBookFromFileUnformatted(EntityPlayer player, File file)
 	{
 		NBTTagCompound tag = new NBTTagCompound();
@@ -896,6 +970,65 @@ public final class FunctionHelper
 
 		tag.setString("author", "ForgeEssentials");
 		tag.setString("title", file.getName().replace(".txt", ""));
+		tag.setTag("pages", pages);
+
+		ItemStack is = new ItemStack(Item.writtenBook);
+		is.setTagCompound(tag);
+		player.inventory.addItemStackToInventory(is);
+	}
+
+	public static void getBookFromFileUnformatted(EntityPlayer player, File file, String title)
+	{
+		NBTTagCompound tag = new NBTTagCompound();
+		NBTTagList pages = new NBTTagList();
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		if (file.isFile())
+		{
+			if (file.getName().contains(".txt"))
+			{
+				List<String> lines = new ArrayList<String>();
+				try
+				{
+					FileInputStream stream = new FileInputStream(file);
+					InputStreamReader streamReader = new InputStreamReader(stream);
+					BufferedReader reader = new BufferedReader(streamReader);
+					String line = reader.readLine();
+					while (line != null)
+					{
+						lines.add(line);
+						line = reader.readLine();
+					}
+					reader.close();
+					streamReader.close();
+					stream.close();
+
+				}
+				catch (Exception e)
+				{
+					OutputHandler.felog.warning("Error reading book: " + file.getName());
+				}
+				while (lines.size() != 0)
+				{
+					String temp = "";
+					for (int i = 0; i < 10 && lines.size() > 0; i++)
+					{
+						temp += lines.get(0) + "\n";
+						lines.remove(0);
+					}
+					map.put("", temp);
+				}
+			}
+		}
+
+		SortedSet<String> keys = new TreeSet<String>(map.keySet());
+		for (String name : keys)
+		{
+			pages.appendTag(new NBTTagString("", name + map.get(name)));
+		}
+
+		tag.setString("author", "ForgeEssentials");
+		tag.setString("title", title);
 		tag.setTag("pages", pages);
 
 		ItemStack is = new ItemStack(Item.writtenBook);
@@ -976,6 +1109,86 @@ public final class FunctionHelper
 
 		tag.setString("author", "ForgeEssentials");
 		tag.setString("title", folder.getName());
+		tag.setTag("pages", pages);
+
+		ItemStack is = new ItemStack(Item.writtenBook);
+		is.setTagCompound(tag);
+		player.inventory.addItemStackToInventory(is);
+	}
+
+	public static void getBookFromFolder(EntityPlayer player, File folder, String title)
+	{
+		NBTTagCompound tag = new NBTTagCompound();
+		NBTTagList pages = new NBTTagList();
+
+		HashMap<String, String> map = new HashMap<String, String>();
+
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles)
+		{
+			if (file.isFile())
+			{
+				if (file.getName().contains(".txt"))
+				{
+					List<String> lines = new ArrayList<String>();
+					try
+					{
+						lines.add(FEChatFormatCodes.GREEN + "START" + FEChatFormatCodes.BLACK);
+						lines.add("");
+						FileInputStream stream = new FileInputStream(file);
+						InputStreamReader streamReader = new InputStreamReader(stream);
+						BufferedReader reader = new BufferedReader(streamReader);
+						String line = reader.readLine();
+						while (line != null)
+						{
+							while (line.length() > 21)
+							{
+								lines.add(line.substring(0, 20));
+								line = line.substring(20);
+							}
+							lines.add(line);
+							line = reader.readLine();
+						}
+						reader.close();
+						streamReader.close();
+						stream.close();
+						lines.add("");
+						lines.add(FEChatFormatCodes.RED + "END" + FEChatFormatCodes.BLACK);
+
+					}
+					catch (Exception e)
+					{
+						OutputHandler.felog.warning("Error reading script: " + file.getName());
+					}
+					int part = 0;
+					int parts = lines.size() / 10 + 1;
+					String filename = file.getName().replaceAll(".txt", "");
+					if (filename.length() > 13)
+						filename = filename.substring(0, 10) + "...";
+					while (lines.size() != 0)
+					{
+						part++;
+						String temp = "";
+						for (int i = 0; i < 10 && lines.size() > 0; i++)
+						{
+							temp += lines.get(0) + "\n";
+							lines.remove(0);
+						}
+						map.put(FEChatFormatCodes.GOLD + " File: " + FEChatFormatCodes.GREY + filename + FEChatFormatCodes.DARKGREY + "\nPart " + part + " of " + parts + FEChatFormatCodes.BLACK + "\n\n", temp);
+					}
+				}
+			}
+		}
+
+		SortedSet<String> keys = new TreeSet<String>(map.keySet());
+		for (String name : keys)
+		{
+			pages.appendTag(new NBTTagString("", name + map.get(name)));
+		}
+
+		tag.setString("author", "ForgeEssentials");
+		tag.setString("title", title);
 		tag.setTag("pages", pages);
 
 		ItemStack is = new ItemStack(Item.writtenBook);
