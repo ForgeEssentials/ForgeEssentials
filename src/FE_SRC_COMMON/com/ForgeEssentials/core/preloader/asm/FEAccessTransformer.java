a4dfc704eec0e3dffc0c5bd8edffb752d86a3e0f
Lucas Tan <luacs1998@live.com>
2013-09-28 16:00:19 +0800

    Change: Simplify code in AT.

--- src/FE_SRC_COMMON/com/ForgeEssentials/core/preloader/asm/FEAccessTransformer.java
+++ src/FE_SRC_COMMON/com/ForgeEssentials/core/preloader/asm/FEAccessTransformer.java
@@ -1,63 +1,15 @@
 package com.ForgeEssentials.core.preloader.asm;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
 import cpw.mods.fml.common.asm.transformers.AccessTransformer;
 
 public class FEAccessTransformer extends AccessTransformer
 {
-	private static FEAccessTransformer	instance;
-	private static List<String>			mapFiles	= new LinkedList<String>();
 
-	public FEAccessTransformer() throws IOException
-	{
-		super();
-		instance = this;
-		// add your access transformers here!
-		mapFiles.add("forgeessentials_at.cfg");
-		Iterator<String> it = mapFiles.iterator();
-		while (it.hasNext())
-		{
-			String file = it.next();
-			readMapFile(file);
-		}
-
+	public FEAccessTransformer() throws IOException {
+		super("forgeessentials_at.cfg");
+		// TODO Auto-generated constructor stub
 	}
-
-	public static void addTransformerMap(String mapFileName)
-	{
-		if (instance == null)
-		{
-			mapFiles.add(mapFileName);
-		}
-		else
-		{
-			instance.readMapFile(mapFileName);
-		}
-	}
-
-	private void readMapFile(String name)
-	{
-		System.out.println("Adding AT map: " + name);
-		try
-		{
-			// get a method from AccessTransformer
-			Method e = AccessTransformer.class.getDeclaredMethod("readMapFile", new Class[]
-			{ String.class });
-			e.setAccessible(true);
-			// run it with the file given.
-			e.invoke(this, new Object[]
-			{ name });
-
-		}
-		catch (Exception ex)
-		{
-			throw new RuntimeException(ex);
-		}
-	}
-
+	
 }
