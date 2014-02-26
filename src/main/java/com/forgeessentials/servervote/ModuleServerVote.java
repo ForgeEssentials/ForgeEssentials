80c4257d54de4f8cafd9b047743830180961bf0e
luacs1998 <luacs1998@users.noreply.github.com>
2014-02-26 17:35:54 +0800

    Add back servervote Please do not name modules in camelcase. It is annoying.

---
+++ src/main/java/com/forgeessentials/servervote/ModuleServerVote.java
@@ -0,0 +1,212 @@
+package com.forgeessentials.servervote;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.Packet3Chat;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.EventPriority;
+import net.minecraftforge.event.ForgeSubscribe;
+
+import com.forgeessentials.api.snooper.VoteEvent;
+import com.forgeessentials.core.ForgeEssentials;
+import com.forgeessentials.core.moduleLauncher.FEModule;
+import com.forgeessentials.core.moduleLauncher.FEModule.Config;
+import com.forgeessentials.core.moduleLauncher.FEModule.Init;
+import com.forgeessentials.core.moduleLauncher.FEModule.ModuleDir;
+import com.forgeessentials.core.moduleLauncher.FEModule.ServerInit;
+import com.forgeessentials.core.moduleLauncher.FEModule.ServerStop;
+import com.forgeessentials.servervote.Votifier.VoteReceiver;
+import com.forgeessentials.util.ChatUtils;
+import com.forgeessentials.util.FunctionHelper;
+import com.forgeessentials.util.OutputHandler;
+import com.forgeessentials.util.events.modules.FEModuleInitEvent;
+import com.forgeessentials.util.events.modules.FEModuleServerInitEvent;
+import com.forgeessentials.util.events.modules.FEModuleServerStopEvent;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.IPlayerTracker;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@FEModule(name = "ServerVoteModule", parentMod = ForgeEssentials.class, configClass = ConfigServerVote.class)
+public class ModuleServerVote implements IPlayerTracker
+{
+	@Config
+	public static ConfigServerVote     config;
+
+	@ModuleDir
+	public static File                 moduleDir;
+	
+	public static VoteReceiver         votifier;
+	public static PrintWriter          log;
+
+    private HashMap<String, VoteEvent>  offlineList = new HashMap<String, VoteEvent>();
+
+	public ModuleServerVote()
+	{
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@Init
+	public void init(FEModuleInitEvent e)
+	{
+	    GameRegistry.registerPlayerTracker(this);
+	}
+
+	@ServerInit
+	public void serverStarting(FEModuleServerInitEvent e)
+	{
+		try
+		{
+			votifier = new VoteReceiver(config.hostname, config.port);
+			votifier.start();
+		}
+		catch (Exception e1)
+		{
+			FMLLog.severe("Error initializing Votifier compat.");
+			FMLLog.severe(e.toString());
+			e1.printStackTrace();
+		}
+		
+		try
+        {
+		    File logFile = new File(e.getModuleDir(), "vote.log");
+		    if (!logFile.exists()) logFile.createNewFile();
+            log = new PrintWriter(logFile);
+        }
+        catch (Exception e1)
+        {
+            e1.printStackTrace();
+        }
+		
+		try
+		{
+		    File file = new File(moduleDir, "offlineVoteList.txt");
+		    if (file.exists())
+		    {
+		        BufferedReader br = new BufferedReader(new FileReader(file));
+	            String line;
+	            while ((line = br.readLine()) != null) 
+	            {
+	                if (!(line.startsWith("#") || line.isEmpty()))
+	                {
+	                    VoteEvent vote = new VoteEvent(line.trim());
+	                    offlineList.put(vote.player, vote);
+	                }
+	            }
+	            br.close();    
+		    }
+		}
+		catch (Exception e1)
+        {
+            e1.printStackTrace();
+        }
+	}
+
+	@ServerStop
+	public void serverStopping(FEModuleServerStopEvent e)
+	{
+		try
+		{
+			votifier.shutdown();
+		}
+		catch (Exception e1)
+		{
+			FMLLog.severe("Error closing Votifier compat thread.");
+			FMLLog.severe(e.toString());
+			e1.printStackTrace();
+		}
+		
+		try
+        {
+            log.close();
+        }
+        catch (Exception e1)
+        {
+            e1.printStackTrace();
+        }
+		
+		try
+		{
+		    File file = new File(moduleDir, "offlineVoteList.txt");
+		    if (!file.exists()) file.createNewFile();
+		    
+		    PrintWriter pw = new PrintWriter(file);
+		    for (VoteEvent vote : offlineList.values())
+		        pw.println(vote.toString());
+		    pw.close();
+		}
+		catch (Exception e1)
+		{
+		    e1.printStackTrace();
+		}
+	}
+
+	@ForgeSubscribe(priority = EventPriority.HIGHEST)
+	public void defVoteResponces(VoteEvent vote)
+	{
+	    EntityPlayerMP player = FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().getPlayerForUsername(vote.player);
+		if (player != null) doPlayer(player, vote);
+		else offlineList.put(vote.player, vote);
+	}
+
+    public static void log(VoteEvent vote)
+    {
+        OutputHandler.felog.finer("Got Vote from player " + vote.player + " by service " + vote.serviceName + " time " + vote.timeStamp);
+    }
+
+    @Override
+    public void onPlayerLogin(EntityPlayer player)
+    {
+        if (offlineList.containsKey(player.username))
+            doPlayer(player, offlineList.remove(player.username));
+    }
+
+    @Override
+    public void onPlayerLogout(EntityPlayer player)
+    {
+        
+    }
+
+    @Override
+    public void onPlayerChangedDimension(EntityPlayer player)
+    {
+        
+    }
+
+    @Override
+    public void onPlayerRespawn(EntityPlayer player)
+    {
+        
+    }
+    
+    private void doPlayer(EntityPlayer player, VoteEvent vote)
+    {
+        if (!config.msgAll.equals(""))
+        {
+            FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager()
+                    .sendPacketToAllPlayers(new Packet3Chat(FunctionHelper.formatColors(config.msgAll.replaceAll("%service", vote.serviceName).replaceAll("%player", vote.player))));
+        }
+
+        if (!config.msgVoter.equals(""))
+        {
+            ChatUtils.sendMessage(player, FunctionHelper.formatColors(config.msgAll.replaceAll("%service", vote.serviceName).replaceAll("%player", vote.player)));
+        }
+
+        if (!config.freeStuff.isEmpty())
+        {
+            for (ItemStack stack : config.freeStuff)
+            {
+                OutputHandler.felog.finer(stack.toString());
+                player.inventory.addItemStackToInventory(stack.copy());
+            }
+        }
+    }
+}
---
+++ src/main/java/com/forgeessentials/servervote/Votifier/VoteReceiver.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright (C) 2012 Vex Software LLC
+ * This file -was- part of Votifier.
+ * 
+ * Votifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Votifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Votifier.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.forgeessentials.servervote.Votifier;
+
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.MinecraftForge;
+
+import com.forgeessentials.api.snooper.VoteEvent;
+import com.forgeessentials.servervote.ModuleServerVote;
+import com.forgeessentials.util.FunctionHelper;
+import com.forgeessentials.util.OutputHandler;
+
+import cpw.mods.fml.common.FMLLog;
+
+/**
+ * Like 90% copied from Votifier github: https://github.com/vexsoftware/votifier
+ * I only changed the init code and the event stuff.
+ * @author Dries007
+ */
+public class VoteReceiver extends Thread
+{
+	/** The host to listen on. */
+	private final String	host;
+
+	/** The port to listen on. */
+	private final int		port;
+
+	/** The server socket. */
+	private ServerSocket	server;
+
+	/** The running flag. */
+	private boolean			running	= true;
+
+	public VoteReceiver(String host, int port) throws Exception
+	{
+		if (0 == host.length())
+		{
+			host = "0.0.0.0";
+
+			try
+			{
+				InetAddress var2 = InetAddress.getLocalHost();
+				host = var2.getHostAddress();
+			}
+			catch (UnknownHostException var3)
+			{
+				FMLLog.severe("Unable to determine local host IP, please set server-ip/hostname in the snooper config : " + var3.getMessage());
+			}
+		}
+
+		this.host = host;
+		this.port = port;
+
+		initialize();
+	}
+
+	private void initialize() throws Exception
+	{
+		try
+		{
+			server = new ServerSocket();
+			server.bind(new InetSocketAddress(host, port));
+			OutputHandler.felog.info("Votifier connection handler initialized!");
+		}
+		catch (Exception ex)
+		{
+			FMLLog.severe("Error initializing vote receiver. Please verify that the configured");
+			FMLLog.severe("IP address and port are not already in use. This is a common problem");
+			FMLLog.severe("with hosting services and, if so, you should check with your hosting provider." + ex);
+			throw new Exception(ex);
+		}
+	}
+
+	/**
+	 * Shuts the vote receiver down cleanly.
+	 */
+	public void shutdown()
+	{
+		running = false;
+		if (server == null)
+			return;
+		try
+		{
+			server.close();
+		}
+		catch (Exception ex)
+		{
+			FMLLog.severe("Unable to shut down vote receiver cleanly.");
+		}
+		System.gc();
+	}
+
+	@Override
+	public void run()
+	{
+		while (running)
+		{
+			try
+			{
+				Socket socket = server.accept();
+				socket.setSoTimeout(5000); // Don't hang on slow connections.
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+				InputStream in = socket.getInputStream();
+
+				// Send them our version.
+				writer.write("VOTIFIER FECOMPAT");
+				writer.newLine();
+				writer.flush();
+
+				// Read the 256 byte block.
+				byte[] block = new byte[256];
+				in.read(block, 0, block.length);
+
+				// Decrypt the block.
+				Cipher cipher = Cipher.getInstance("RSA");
+				cipher.init(Cipher.DECRYPT_MODE, ModuleServerVote.config.privateKey);
+				block = cipher.doFinal(block);
+				int position = 0;
+
+				// Perform the opcode check.
+				String opcode = readString(block, position);
+				position += opcode.length() + 1;
+				if (!opcode.equals("VOTE"))
+					// Something went wrong in RSA.
+					throw new Exception("Unable to decode RSA");
+
+				// Parse the block.
+				String serviceName = readString(block, position);
+				position += serviceName.length() + 1;
+				String username = readString(block, position);
+				position += username.length() + 1;
+				String address = readString(block, position);
+				position += address.length() + 1;
+				String timeStamp = readString(block, position);
+				position += timeStamp.length() + 1;
+
+				// Create the vote.
+				VoteEvent vote = new VoteEvent(username, serviceName, address, timeStamp);
+
+				ModuleServerVote.log(vote);
+
+				EntityPlayerMP player = FunctionHelper.getPlayerForName(vote.player);
+		        if (player == null)
+		        {
+		            if (!ModuleServerVote.config.allowOfflineVotes)
+		            {
+		                OutputHandler.felog.finer("Player for vote not online, vote canceled.");
+		                vote.setFeedback("notOnline");
+		                vote.setCanceled(true);
+		                return;
+		            }
+		            else
+		            {
+		                try
+                        {
+                            MinecraftForge.EVENT_BUS.post(vote);
+                        }
+                        catch (Exception e)
+                        {
+                            
+                        }
+		            }
+		        }
+
+				// Clean up.
+				writer.close();
+				in.close();
+				socket.close();
+			}
+			catch (SocketException ex)
+			{
+				FMLLog.severe("Protocol error. Ignoring packet");
+				ex.printStackTrace();
+			}
+			catch (BadPaddingException ex)
+			{
+				FMLLog.severe("Unable to decrypt vote record. Make sure that that your public key matches the one you gave the server list.");
+				ex.printStackTrace();
+			}
+			catch (Exception ex)
+			{
+				FMLLog.severe("Exception caught while receiving a vote notification");
+				ex.printStackTrace();
+			}
+		}
+
+		System.gc();
+	}
+
+	/**
+	 * Reads a string from a block of data.
+	 * @param data
+	 * The data to read from
+	 * @return The string
+	 */
+	private String readString(byte[] data, int offset)
+	{
+		StringBuilder builder = new StringBuilder();
+		for (int i = offset; i < data.length; i++)
+		{
+			if (data[i] == '\n')
+			{
+				break; // Delimiter reached.
+			}
+			builder.append((char) data[i]);
+		}
+		return builder.toString();
+	}
+}
