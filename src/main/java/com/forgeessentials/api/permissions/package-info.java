55e6bb76d54051695bcdbf51c24d4029a2e12b53
luacs1998 <luacs1998@live.com>
2014-06-17 15:46:45 +0800

    Add package-info.java files for our API, to be FML-compliant and use its @API annotation.

---
+++ src/main/java/com/forgeessentials/api/permissions/package-info.java
@@ -0,0 +1,6 @@
+@API(owner = "ForgeEssentials", provides = "ForgeEssentialsAPI|Perms", apiVersion = "1.0")
+
+// to be deprecated as and when Forge gets a Permission API.
+package com.forgeessentials.api.permissions;
+
+import cpw.mods.fml.common.API;
---
+++ src/main/java/com/forgeessentials/api/snooper/package-info.java
@@ -0,0 +1,4 @@
+@API(owner = "ForgeEssentials", provides = "ForgeEssentialsAPI", apiVersion = "1.0")
+package com.forgeessentials.api.snooper;
+
+import cpw.mods.fml.common.API;
