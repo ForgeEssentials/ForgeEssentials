371506d90e69ff71fa08a247215a0788ca404f08
luacs1998 <luacs1998@live.com>
2014-09-08 14:33:13 +0800

    Added Lombok, because the rest requested it. No actual usage though. Also removed PermsAPI, seeing as usage is minimal in 1.6.4 and I can't be bothered to keep backporting it.

--- src/main/java/com/forgeessentials/api/permissions/RegGroup.java
+++ src/main/java/com/forgeessentials/api/permissions/RegGroup.java
@@ -1,7 +1,6 @@
 package com.forgeessentials.api.permissions;
 
 import com.forgeessentials.api.APIRegistry;
-import minecraftforge.permissions.PermissionsManager;
 import net.minecraft.util.EnumChatFormatting;
 
 /**
@@ -15,7 +14,7 @@ public enum RegGroup {
      * zone. All players are part of this group. This group is guaranteed
      * existence
      */
-    ZONE("_ZONE_", " ", " ", null, 0, PermissionsManager.RegisteredPermValue.TRUE)
+    ZONE("_ZONE_", " ", " ", null, 0)
             {
                 @Override
                 public Group getGroup()
@@ -29,33 +28,32 @@ public enum RegGroup {
      * first log in. The players in this group are usually denied commands and
      * breaking blocks before they are promoted to members.
      */
-    GUESTS("Guests", EnumChatFormatting.GRAY + "[GUEST]", " ", null, 0, PermissionsManager.RegisteredPermValue.TRUE),
+    GUESTS("Guests", EnumChatFormatting.GRAY + "[GUEST]", " ", null, 0),
 
     /**
      * This is usually for players that are actually members of the server. They
      * will most likely be able to use basic commands as well as break blocks
      * and stuff in the world.
      */
-    MEMBERS("Members", " ", " ", null, 0, PermissionsManager.RegisteredPermValue.TRUE),
+    MEMBERS("Members", " ", " ", null, 0),
 
     /**
      * This is usually for players that are admins or owners of a given zone
      * They will most likely have WorldEdit access, as well as the power to edit
      * permissions in the zone.
      */
-    ZONE_ADMINS("ZoneAdmins", EnumChatFormatting.RED + "[ZoneAdmin]", " ", null, 0, PermissionsManager.RegisteredPermValue.OP),
+    ZONE_ADMINS("ZoneAdmins", EnumChatFormatting.RED + "[ZoneAdmin]", " ", null, 0),
 
     /**
      * This is automatically assigned to the server owner when they make a world
      * available to the LAN. This is also best kept for players that have direct
      * access to the server's console and filesystem.
      */
-    OWNERS("Owners", EnumChatFormatting.RED + "[OWNER]", " ", null, 999, PermissionsManager.RegisteredPermValue.OP);
+    OWNERS("Owners", EnumChatFormatting.RED + "[OWNER]", " ", null, 999);
 
-    private RegGroup(String name, String parent, String prefix, String suffix, int priority, PermissionsManager.RegisteredPermValue equivalent)
+    private RegGroup(String name, String parent, String prefix, String suffix, int priority)
     {
         this.name = name;
-        this.equivalent = equivalent;
         group = new Group(name, parent, prefix, suffix, APIRegistry.zones.getGLOBAL().getZoneName(), priority);
     }
 
@@ -70,14 +68,8 @@ public enum RegGroup {
         return group;
     }
 
-    public PermissionsManager.RegisteredPermValue getEquivalent()
-    {
-        return equivalent;
-    }
-
     private Group group;
     private String name;
-    private PermissionsManager.RegisteredPermValue equivalent;
 
     public static final String LADDER = "mainLadder";
 
