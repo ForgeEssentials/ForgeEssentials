814f5716f4c02f484ff00d975bdf409e2d80519a
Malkierian <rhydonj@gmail.com>
2014-11-10 23:13:50 -0700

    Groups.json now loads properly.  Also decided to put only global permissions in groups.json in storage root.  World group permissions will go in world/groups.json instead.

--- src/main/java/com/forgeessentials/permissions/persistence/JsonProvider.java
+++ src/main/java/com/forgeessentials/permissions/persistence/JsonProvider.java
@@ -1,6 +1,8 @@
 package com.forgeessentials.permissions.persistence;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.forgeessentials.api.permissions.FEPermissions;
+import com.forgeessentials.api.permissions.IPermissionsHelper;
 import com.forgeessentials.api.permissions.ServerZone;
 import com.forgeessentials.api.permissions.Zone.PermissionList;
 import com.forgeessentials.permissions.core.ZonePersistenceProvider;
@@ -17,33 +20,68 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class JsonProvider extends ZonePersistenceProvider {
-	
+public class JsonProvider extends ZonePersistenceProvider
+{
 	private File path;
+	private Gson gson;
 	
 	public JsonProvider(File path)
 	{
-		this.path = path;		
+		this.path = path;
+		gson = new GsonBuilder()
+				.disableHtmlEscaping()
+				.setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+				.setPrettyPrinting()
+				.create();
 	}
 
 	@Override
 	public ServerZone load()
 	{
-		return null;
+		OutputHandler.felog.info("Loading Json ServerZone");
+		ServerZone serverZone = new ServerZone();
+		loadGroups(serverZone);
+		return serverZone;
+	}
+
+	private void loadGroups(ServerZone serverZone)
+	{
+		GroupsData groupsData = null;
+		try
+		{
+			BufferedReader reader = new BufferedReader(new FileReader(path + "/groups.json"));
+			groupsData = gson.fromJson(reader, GroupsData.class);
+		}
+		catch (IOException e)
+		{
+			OutputHandler.felog.severe("Could not load groups data: " + e.getMessage());
+			return;
+		}
+		for(String group : groupsData.groups.keySet())
+		{
+			GroupData groupData = groupsData.groups.get(group);
+			OutputHandler.felog.info("Loading PermissionList for group " + group);
+			PermissionList list = PermissionList.getPermissionMapFromList(groupData.permissions);
+			list.put(FEPermissions.PREFIX, (groupData.prefix == null ? "" : groupData.prefix));
+			list.put(FEPermissions.SUFFIX, (groupData.suffix == null ? "" : groupData.suffix));
+			list.put(FEPermissions.GROUP_PRIORITY, Integer.toString(groupData.priority));
+			if(groupData.Default)
+			{
+				list.put("fe.internal.group.default", IPermissionsHelper.PERMISSION_TRUE);
+			}
+			serverZone.getOrCreateGroupPermissions(group).putAll(list);
+		}
+//		GroupsData groupsData = gson.f
 	}
 
 	@Override
 	public void save(ServerZone serverZone)
 	{
-		Gson gson = new GsonBuilder()
-				.disableHtmlEscaping()
-				.setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-				.setPrettyPrinting()
-				.create();
-		saveGroups(gson, serverZone);
+		saveGroups(serverZone);
+		saveWorlds(serverZone);
 	}
 	
-	public void saveGroups(Gson gson, ServerZone zone)
+	public void saveGroups(ServerZone zone)
 	{
 		GroupsData groupsData = new GroupsData();
 		for(String group : zone.getGroups())
@@ -55,9 +93,9 @@ public class JsonProvider extends ZonePersistenceProvider {
 			int priority = Integer.parseInt(groupList.get(FEPermissions.GROUP_PRIORITY));
 			List<String> list = groupList.getPermissionsListFromMap();
 			GroupData groupData = new GroupData(prefix, suffix, Default, priority);
-			ZonePerms zonePerms = new ZonePerms(zone.getId(), zone.getName(), zone.getParent().getName());
-			zonePerms.permissions.addAll(list);
-			groupData.zones.add(zonePerms);
+//			List zonePerms = new ZonePerms(zone.getId(), zone.getName(), zone.getParent().getName());
+//			zonePerms.permissions.addAll(list);
+			groupData.permissions.addAll(list);
 			groupsData.groups.put(group, groupData);
 		}
 		String json = gson.toJson(groupsData);
@@ -76,6 +114,11 @@ public class JsonProvider extends ZonePersistenceProvider {
 		}
 	}
 	
+	public void saveWorlds(ServerZone serverZone)
+	{
+		WorldData worldData = new WorldData();
+	}
+	
 	// helper classes
 	public static class GroupsData
 	{
@@ -86,13 +129,14 @@ public class JsonProvider extends ZonePersistenceProvider {
 			groups = new HashMap<String, GroupData>();
 		}
 	}
+	
 	public static class GroupData
 	{
 		public String prefix;
 		public String suffix;
 		public boolean Default;
 		public int priority;
-		public List<ZonePerms> zones;
+		public List<String> permissions;
 		
 		public GroupData(String prefix, String suffix, boolean Default, int priority)
 		{
@@ -100,8 +144,13 @@ public class JsonProvider extends ZonePersistenceProvider {
 			this.suffix = suffix;
 			this.Default = Default;
 			this.priority = priority;
-			zones = new ArrayList<ZonePerms>();
+			permissions = new ArrayList<String>();
 		}
+	}
+	
+	public static class WorldData
+	{
+		
 	}
 	
 	public static class ZonePerms
