e4b5733fd35eb32fd4ea9d62313de560d006c0c1
Geoffrey McRae <geoff@spacevs.com>
2014-11-28 18:54:07 +1100

    [multiworld] Added initial WorldType support and fixed client crash when connecting to custom dim.

--- src/main/java/com/forgeessentials/multiworld/core/MultiworldManager.java
+++ src/main/java/com/forgeessentials/multiworld/core/MultiworldManager.java
@@ -16,9 +16,6 @@ import java.util.Set;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.WorldManager;
 import net.minecraft.world.WorldProvider;
-import net.minecraft.world.WorldProviderEnd;
-import net.minecraft.world.WorldProviderHell;
-import net.minecraft.world.WorldProviderSurface;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.WorldSettings;
 import net.minecraft.world.WorldType;
@@ -27,11 +24,18 @@ import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.WorldEvent;
 
+import cpw.mods.fml.common.network.FMLEmbeddedChannel;
+import cpw.mods.fml.common.network.FMLOutboundHandler;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.relauncher.Side;
+import net.minecraftforge.common.network.ForgeMessage.DimensionRegisterMessage;
+
 import org.apache.commons.io.FileUtils;
 
 import com.forgeessentials.data.v2.DataManager;
 import com.forgeessentials.multiworld.core.exception.MultiworldAlreadyExistsException;
 import com.forgeessentials.multiworld.core.exception.ProviderNotFoundException;
+import com.forgeessentials.multiworld.core.exception.WorldTypeNotFoundException;
 import com.forgeessentials.util.OutputHandler;
 import com.forgeessentials.util.events.ServerEventHandler;
 
@@ -40,23 +44,14 @@ import cpw.mods.fml.common.gameevent.TickEvent.ServerTickEvent;
 
 public class MultiworldManager extends ServerEventHandler {
 
-    public static final String PROVIDER_DEFAULT = "default";
-    public static final String PROVIDER_FLAT = "flat";
-    public static final String PROVIDER_AMPLIFIED = "amp";
-    public static final String PROVIDER_LARGE_BIOMES = "large";
-    public static final String PROVIDER_HELL = "nether";
-    public static final String PROVIDER_END = "end";
-    public static final String PROVIDER_CUSTOM = "custom";
-    public static final String PROVIDER_CUSTOM_HELL = "custom_nether";
-    public static final String PROVIDER_CUSTOM_END = "custom_end";
+    public static final String PROVIDER_NORMAL = "normal";
+    public static final String PROVIDER_HELL   = "nether";
+    public static final String PROVIDER_END    = "end";
     
     public static final String[] PROVIDERS = new String[] {
-        PROVIDER_DEFAULT,
-        PROVIDER_FLAT,
-        PROVIDER_AMPLIFIED,
-        PROVIDER_LARGE_BIOMES,
+        PROVIDER_NORMAL,
         PROVIDER_HELL,
-        PROVIDER_END,
+        PROVIDER_END
     };
 
     // ============================================================
@@ -81,6 +76,11 @@ public class MultiworldManager extends ServerEventHandler {
     protected Map<String, Integer> worldProviderClasses = new HashMap<String, Integer>();
 
     /**
+     * Mapping from worldType names to WorldType objects
+     */
+    protected Map<String, WorldType> worldTypes = new HashMap<String, WorldType>();
+
+    /**
      * List of worlds that have been marked for deletion
      */
     protected ArrayList<WorldServer> worldsToDelete = new ArrayList<WorldServer>();
@@ -89,6 +89,11 @@ public class MultiworldManager extends ServerEventHandler {
      * List of worlds that have been marked for removal
      */
     protected ArrayList<WorldServer> worldsToRemove = new ArrayList<WorldServer>();
+
+    /**
+     * Event handler for new clients that need to know about our worlds
+     */
+    protected MultiworldEventHandler eventHandler = new MultiworldEventHandler(this);
 
     // ============================================================
 
@@ -115,6 +120,10 @@ public class MultiworldManager extends ServerEventHandler {
             {
                 OutputHandler.felog.severe("Provider with name \"" + world.provider + "\" not found!");
             }
+            catch (WorldTypeNotFoundException e)
+            {
+                OutputHandler.felog.severe("WorldType with name \"" + world.worldType + "\" not found!");
+            }
         }
     }
 
@@ -138,7 +147,7 @@ public class MultiworldManager extends ServerEventHandler {
         return worlds.get(name);
     }
 
-    public void addWorld(Multiworld world) throws ProviderNotFoundException, MultiworldAlreadyExistsException
+    public void addWorld(Multiworld world) throws ProviderNotFoundException, WorldTypeNotFoundException, MultiworldAlreadyExistsException
     {
         if (worlds.containsKey(world.getName()))
             throw new MultiworldAlreadyExistsException();
@@ -147,18 +156,20 @@ public class MultiworldManager extends ServerEventHandler {
         world.save();
     }
 
-    protected void loadWorld(Multiworld world) throws ProviderNotFoundException
+    protected void loadWorld(Multiworld world) throws ProviderNotFoundException, WorldTypeNotFoundException
     {
         if (world.worldLoaded)
             return;
         try {
+            OutputHandler.felog.info("WorldType: " + world.worldType);
             initializeMultiworldProvider(world);
+            world.worldTypeObj = getWorldTypeByName(world.worldType);
             
             // Register dimension with last used id if possible
             if (DimensionManager.isDimensionRegistered(world.dimensionId))
-            {
                 world.dimensionId = DimensionManager.getNextFreeDimId();
-            }
+
+            // Register the dimension
             DimensionManager.registerDimension(world.dimensionId, world.providerId);
             worldsByDim.put(world.dimensionId, world);
     
@@ -168,7 +179,7 @@ public class MultiworldManager extends ServerEventHandler {
             if (overworld == null)
                 throw new RuntimeException("Cannot hotload dim: Overworld is not Loaded!");
             ISaveHandler savehandler = new MultiworldSaveHandler(overworld.getSaveHandler(), world);
-            WorldSettings worldSettings = new WorldSettings(world.seed, world.gameType, world.mapFeaturesEnabled, false, world.worldType);
+            WorldSettings worldSettings = new WorldSettings(world.seed, world.gameType, world.mapFeaturesEnabled, false, world.worldTypeObj);
     
             // Create WorldServer with settings
             WorldServer worldServer = new WorldServerMultiworld(server, savehandler, //
@@ -181,6 +192,16 @@ public class MultiworldManager extends ServerEventHandler {
     
             // Post WorldEvent.Load
             MinecraftForge.EVENT_BUS.post(new WorldEvent.Load(worldServer));
+
+            // This is required otherwise the S01PacketJoinGame.worldinfo may not be initialized
+            worldServer.getWorldInfo().setGameType(world.gameType);
+            server.func_147139_a(server.func_147135_j());
+
+            // Tell everyone about the new dim
+            FMLEmbeddedChannel channel = NetworkRegistry.INSTANCE.getChannel("FORGE", Side.SERVER);
+            DimensionRegisterMessage msg = new DimensionRegisterMessage(world.dimensionId, world.providerId);
+            channel.attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.ALL);
+            channel.writeOutbound(msg);
         }
         catch (Exception e)
         {
@@ -191,42 +212,20 @@ public class MultiworldManager extends ServerEventHandler {
 
     public void initializeMultiworldProvider(Multiworld world) throws ProviderNotFoundException
     {
-        world.worldType = WorldType.DEFAULT;
         switch (world.provider.toLowerCase())
         {
-        case PROVIDER_DEFAULT:
-            world.providerId = getProviderIDByClass(WorldProviderSurface.class.getName());
-            break;
-        case PROVIDER_FLAT:
-            world.providerId = getProviderIDByClass(WorldProviderSurface.class.getName());
-            world.worldType = WorldType.FLAT;
-            break;
-        case PROVIDER_AMPLIFIED:
-            world.providerId = getProviderIDByClass(WorldProviderSurface.class.getName());
-            world.worldType = WorldType.AMPLIFIED;
-            break;
-        case PROVIDER_LARGE_BIOMES:
-            world.providerId = getProviderIDByClass(WorldProviderSurface.class.getName());
-            world.worldType = WorldType.LARGE_BIOMES;
+        // We use the hardcoded values as some mods outright replace the class (BiomesOPlenty)
+        case PROVIDER_NORMAL:        
+            world.providerId = 0;
             break;
         case PROVIDER_HELL:
-            world.providerId = getProviderIDByClass(WorldProviderHell.class.getName());
+            world.providerId = -1;
             break;
         case PROVIDER_END:
-            world.providerId = getProviderIDByClass(WorldProviderEnd.class.getName());
+            world.providerId = 1;
             break;
-        case PROVIDER_CUSTOM:
-            world.providerId = getProviderIDByClass(WorldProviderSurface.class.getName());
-            world.worldType = WORLD_TYPE_MULTIWORLD;
-            throw new ProviderNotFoundException();
-        case PROVIDER_CUSTOM_HELL:
-            world.providerId = getProviderIDByClass(WorldProviderHell.class.getName());
-            world.worldType = WORLD_TYPE_MULTIWORLD;
-            throw new ProviderNotFoundException();
-        case PROVIDER_CUSTOM_END:
-            world.providerId = getProviderIDByClass(WorldProviderEnd.class.getName());
-            world.worldType = WORLD_TYPE_MULTIWORLD;
-            throw new ProviderNotFoundException();
+
+        // Otherwise we try to use the provider classname that was supplied
         default:
             world.providerId = getProviderIDByClass(world.provider);
             break;
@@ -385,7 +384,13 @@ public class MultiworldManager extends ServerEventHandler {
             @SuppressWarnings("unchecked")
             Hashtable<Integer, Class<? extends WorldProvider>> loadedProviders = (Hashtable<Integer, Class<? extends WorldProvider>>) f_providers.get(null);
             for (Entry<Integer, Class<? extends WorldProvider>> provider : loadedProviders.entrySet())
+            {
+                // skip the default providers as these are aliased as 'normal', 'nether' and 'end'
+                if (provider.getKey() >= -1 && provider.getKey() <= 1)
+                    continue;
+
                 worldProviderClasses.put(provider.getValue().getName(), provider.getKey());
+            }
         }
         catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e)
         {
@@ -394,7 +399,7 @@ public class MultiworldManager extends ServerEventHandler {
         OutputHandler.felog.info("[Multiworld] Available world providers:");
         for (Entry<String, Integer> provider : worldProviderClasses.entrySet())
         {
-            OutputHandler.felog.info("#" + provider.getValue() + ":" + provider.getKey());
+            OutputHandler.felog.info("# " + provider.getValue() + ":" + provider.getKey());
         }
     }
 
@@ -403,4 +408,36 @@ public class MultiworldManager extends ServerEventHandler {
         return worldProviderClasses;
     }
 
+    // ============================================================
+    // WorldType management
+
+    /**
+     * Returns the WorldType for a given worldType string
+     */
+    public WorldType getWorldTypeByName(String worldType) throws WorldTypeNotFoundException
+    {
+        WorldType type = worldTypes.get(worldType.toUpperCase());
+        if (type == null)
+            throw new WorldTypeNotFoundException();
+        return type;
+    }
+
+    /**
+     * Builds the map of valid worldTypes
+     */
+    public void loadWorldTypes()
+    {
+        for (int i = 0; i < WorldType.worldTypes.length; ++i)
+            if (WorldType.worldTypes[i] != null)
+                worldTypes.put(WorldType.worldTypes[i].getWorldTypeName().toUpperCase(), WorldType.worldTypes[i]);
+
+        OutputHandler.felog.info("[Multiworld] Available world types:");
+        for (Entry<String, WorldType> worldType: worldTypes.entrySet())
+            OutputHandler.felog.info("# " + worldType.getKey());
+    }
+
+    public Map<String, WorldType> getWorldTypes()
+    {
+        return worldTypes;
+    }
 }
--- src/main/java/com/forgeessentials/multiworld/core/WorldServerMultiworld.java
+++ src/main/java/com/forgeessentials/multiworld/core/WorldServerMultiworld.java
@@ -3,21 +3,23 @@ package com.forgeessentials.multiworld.core;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.MinecraftException;
+import net.minecraft.world.WorldServerMulti;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.WorldSettings;
+import net.minecraft.world.storage.DerivedWorldInfo;
 import net.minecraft.world.storage.ISaveHandler;
 
-public class WorldServerMultiworld extends WorldServer {
+public class WorldServerMultiworld extends WorldServerMulti {
     
     private Multiworld world;
 
     public WorldServerMultiworld(MinecraftServer mcServer, ISaveHandler saveHandler, String worldname, int dimensionId, WorldSettings worldSettings,
             WorldServer worldServer, Profiler profiler, Multiworld world)
     {
-        super(mcServer, saveHandler, worldname, dimensionId, worldSettings, profiler);
+        super(mcServer, saveHandler, worldname, dimensionId, worldSettings, worldServer, profiler);
         this.mapStorage = worldServer.mapStorage;
         this.worldScoreboard = worldServer.getScoreboard();
-        // this.worldInfo = new DerivedWorldInfo(worldInfo);
+        this.worldInfo = new DerivedWorldInfo(worldInfo);
         this.world = world;
     }
 
