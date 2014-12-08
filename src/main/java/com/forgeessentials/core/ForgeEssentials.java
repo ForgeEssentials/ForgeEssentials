92ebe27a4aacf13c46aa90d76ecf042bfc746c1d
luacs1998 <luacs1998@users.noreply.github.com>
2014-12-08 10:07:26 +0800

    Update Forge and Forge requirement to build 1258. Devs, re-setup your workspaces!

--- src/main/java/com/forgeessentials/core/ForgeEssentials.java
+++ src/main/java/com/forgeessentials/core/ForgeEssentials.java
@@ -76,7 +76,7 @@ import cpw.mods.fml.relauncher.Side;
  * Main mod class
  */
 
-@Mod(modid = "ForgeEssentials", name = "Forge Essentials", version = FEModContainer.version, acceptableRemoteVersions = "*", dependencies = "required-after:Forge@[10.13.1.1219,)")
+@Mod(modid = "ForgeEssentials", name = "Forge Essentials", version = FEModContainer.version, acceptableRemoteVersions = "*", dependencies = "required-after:Forge@[10.13.2.1258,)")
 public class ForgeEssentials extends ConfigLoaderBase {
 
     public static final String CONFIG_CAT = "Core";
