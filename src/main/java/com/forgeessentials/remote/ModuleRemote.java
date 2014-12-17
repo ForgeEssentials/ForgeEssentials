9c5b1ec0f069b4fc1afd1bfdde01d4b116f0d5e8
helinus <admin@helinus.se>
2014-12-17 17:32:30 +0100

    Making some stuff configurable in ModuleRemote

--- src/main/java/com/forgeessentials/remote/ModuleRemote.java
+++ src/main/java/com/forgeessentials/remote/ModuleRemote.java
@@ -5,25 +5,28 @@ import java.security.GeneralSecurityException;
 
 import com.forgeessentials.core.ForgeEssentials;
 import com.forgeessentials.core.moduleLauncher.FEModule;
+import com.forgeessentials.core.moduleLauncher.config.IConfigLoader.ConfigLoaderBase;
 import com.forgeessentials.util.OutputHandler;
 import com.forgeessentials.util.events.FEModuleEvent.FEModuleServerInitEvent;
 import com.forgeessentials.util.events.FEModuleEvent.FEModuleServerStopEvent;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.common.config.Configuration;
 
 @FEModule(name = "Remote", parentMod = ForgeEssentials.class, canDisable = true)
-public class ModuleRemote {
+public class ModuleRemote extends ConfigLoaderBase {
 
+    private static final String CONFIG_CAT = "Remote";
     private RemoteServer server;
+    private int port = 27020;
+    private String hostname = "localhost";
+    private boolean useSSL = false;
 
     @SubscribeEvent
     public void serverStarting(FEModuleServerInitEvent e)
     {
         try
         {
-            int port = 27020;
-            String hostname = "localhost";
-            boolean useSSL = false;
             if (useSSL)
             {
                 SSLContextHelper sslCtxHelper = new SSLContextHelper();
@@ -49,6 +52,13 @@ public class ModuleRemote {
             server.close();
             server = null;
         }
+    }
+    @Override
+    public void load(Configuration config, boolean isReload)
+    {
+        hostname = config.get(CONFIG_CAT, "hostname", "localhost", "Hostname of the minecraft server").getString();
+        port = config.get(CONFIG_CAT, "port", 27020, "Port to connect remotes to").getInt();
+        // useSSL = config.get(CONFIG_CAT, "useSSL", false, "Protect the communication with SSL").getBoolean();
     }
 
 }
