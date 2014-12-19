66450c5650ea902669a4bdfa7b97b0bf258eda46
Geoffrey McRae <geoff@spacevs.com>
2014-12-19 23:13:00 +1100

    [worldborder] formatting cosmetics

--- src/main/java/com/forgeessentials/worldborder/TickTaskFill.java
+++ src/main/java/com/forgeessentials/worldborder/TickTaskFill.java
@@ -32,271 +32,291 @@ import com.forgeessentials.util.tasks.TaskRegistry;
  */
 
 @SaveableObject
-public class TickTaskFill implements ITickTask {
-    final ClassContainer con = new ClassContainer(TickTaskFill.class);
-    @SaveableField
-    public int speed = 1;
-    public WorldBorder border;
-    @UniqueLoadingKey
-    @SaveableField
-    String dimID;
-    boolean isComplete = false;
-    WorldServer world;
-    int minX;
-    int minZ;
-    int maxX;
-    int maxZ;
-    int centerX;
-    int centerZ;
-    int rad;
-    @SaveableField
-    long ticks = 0L;
-    @SaveableField
-    long todo = 0L;
-    MinecraftServer server = MinecraftServer.getServer();
-    ICommandSender source;
-    boolean stopped = false;
-    @SaveableField
-    private int X;
-    @SaveableField
-    private int Z;
+public class TickTaskFill implements ITickTask
+{
+	final ClassContainer con = new ClassContainer(TickTaskFill.class);
+	@SaveableField
+	public int speed = 1;
+	public WorldBorder border;
+	@UniqueLoadingKey
+	@SaveableField
+	String dimID;
+	boolean isComplete = false;
+	WorldServer world;
+	int minX;
+	int minZ;
+	int maxX;
+	int maxZ;
+	int centerX;
+	int centerZ;
+	int rad;
+	@SaveableField
+	long ticks = 0L;
+	@SaveableField
+	long todo = 0L;
+	MinecraftServer server = MinecraftServer.getServer();
+	ICommandSender source;
+	boolean stopped = false;
+	@SaveableField
+	private int X;
+	@SaveableField
+	private int Z;
 
-    private TickTaskFill(IReconstructData tag)
-    {
-        X = (Integer) tag.getFieldValue("X");
-        Z = (Integer) tag.getFieldValue("Z");
-        speed = (Integer) tag.getFieldValue("speed");
-        ticks = (Long) tag.getFieldValue("ticks");
-        todo = (Long) tag.getFieldValue("todo");
-    }
+	private TickTaskFill(IReconstructData tag)
+	{
+		X = (Integer) tag.getFieldValue("X");
+		Z = (Integer) tag.getFieldValue("Z");
+		speed = (Integer) tag.getFieldValue("speed");
+		ticks = (Long) tag.getFieldValue("ticks");
+		todo = (Long) tag.getFieldValue("todo");
+	}
 
-    public TickTaskFill(WorldServer worldToFill, ICommandSender sender, boolean restart)
-    {
-        dimID = worldToFill.provider.dimensionId + "";
-        FEChunkLoader.instance().forceLoadWorld(worldToFill);
+	public TickTaskFill(WorldServer worldToFill, ICommandSender sender,
+			boolean restart)
+	{
+		dimID = worldToFill.provider.dimensionId + "";
+		FEChunkLoader.instance().forceLoadWorld(worldToFill);
 
-        if (CommandFiller.map.containsKey(worldToFill.provider.dimensionId))
-        {
-            OutputHandler.chatError(server, "Already running a filler for dim " + dimID + "!");
-            return;
-        }
+		if (CommandFiller.map.containsKey(worldToFill.provider.dimensionId))
+		{
+			OutputHandler.chatError(server, "Already running a filler for dim "
+					+ dimID + "!");
+			return;
+		}
 
-        source = sender;
-        world = worldToFill;
-        border = ModuleWorldBorder.borderMap.get(APIRegistry.perms.getWorldZone(world).getName());
+		source = sender;
+		world = worldToFill;
+		border = ModuleWorldBorder.borderMap.get(APIRegistry.perms
+				.getWorldZone(world).getName());
 
-        if (border.shapeByte == 0 || border.rad == 0)
-        {
-            OutputHandler.chatError(sender, "You need to set the worldborder first!");
-            return;
-        }
+		if (border.shapeByte == 0 || border.rad == 0)
+		{
+			OutputHandler.chatError(sender,
+					"You need to set the worldborder first!");
+			return;
+		}
 
-        X = minX = (border.center.getX() - border.rad - ModuleWorldBorder.overGenerate) / 16;
-        Z = minZ = (border.center.getZ() - border.rad - ModuleWorldBorder.overGenerate) / 16;
-        maxX = (border.center.getX() + border.rad + ModuleWorldBorder.overGenerate) / 16;
-        maxZ = (border.center.getZ() + border.rad + ModuleWorldBorder.overGenerate) / 16;
-        centerX = border.center.getX() / 16;
-        centerZ = border.center.getZ() / 16;
-        rad = (border.rad + ModuleWorldBorder.overGenerate) / 16;
+		X = minX = (border.center.getX() - border.rad - ModuleWorldBorder.overGenerate) / 16;
+		Z = minZ = (border.center.getZ() - border.rad - ModuleWorldBorder.overGenerate) / 16;
+		maxX = (border.center.getX() + border.rad + ModuleWorldBorder.overGenerate) / 16;
+		maxZ = (border.center.getZ() + border.rad + ModuleWorldBorder.overGenerate) / 16;
+		centerX = border.center.getX() / 16;
+		centerZ = border.center.getZ() / 16;
+		rad = (border.rad + ModuleWorldBorder.overGenerate) / 16;
 
-        todo = border.getETA();
+		todo = border.getETA();
 
-        OutputHandler.debug("Filler for :" + world.provider.dimensionId);
-        OutputHandler.debug("MinX=" + minX + " MaxX=" + maxX);
-        OutputHandler.debug("MinZ=" + minZ + " MaxZ=" + maxZ);
+		OutputHandler.debug("Filler for :" + world.provider.dimensionId);
+		OutputHandler.debug("MinX=" + minX + " MaxX=" + maxX);
+		OutputHandler.debug("MinZ=" + minZ + " MaxZ=" + maxZ);
 
-        if (restart)
-        {
-            TickTaskFill saved = DataManager.getInstance().load(TickTaskFill.class, Integer.toString(worldToFill.provider.dimensionId));
-            if (saved == null)
-                saved = (TickTaskFill) DataStorageManager.getReccomendedDriver().loadObject(con, Integer.toString(worldToFill.provider.dimensionId));
-            if (saved != null)
-            {
-                OutputHandler.chatWarning(source, "Found a stopped filler. Will resume that one.");
-                X = saved.X;
-                Z = saved.Z;
-                speed = saved.speed;
-                ticks = saved.ticks;
-                todo = saved.todo;
-            }
-        }
+		if (restart)
+		{
+			TickTaskFill saved = DataManager.getInstance().load(
+					TickTaskFill.class,
+					Integer.toString(worldToFill.provider.dimensionId));
+			if (saved == null)
+				saved = (TickTaskFill) DataStorageManager
+						.getReccomendedDriver()
+						.loadObject(
+								con,
+								Integer.toString(worldToFill.provider.dimensionId));
+			if (saved != null)
+			{
+				OutputHandler.chatWarning(source,
+						"Found a stopped filler. Will resume that one.");
+				X = saved.X;
+				Z = saved.Z;
+				speed = saved.speed;
+				ticks = saved.ticks;
+				todo = saved.todo;
+			}
+		}
 
-        TaskRegistry.registerTask(this);
+		TaskRegistry.registerTask(this);
 
-        OutputHandler.chatWarning(source, "This filler will take about " + getETA() + " at current speed.");
-    }
+		OutputHandler.chatWarning(source, "This filler will take about "
+				+ getETA() + " at current speed.");
+	}
 
-    @Reconstructor
-    private static TickTaskFill reconstruct(IReconstructData tag)
-    {
-        return new TickTaskFill(tag);
-    }
+	@Reconstructor
+	private static TickTaskFill reconstruct(IReconstructData tag)
+	{
+		return new TickTaskFill(tag);
+	}
 
-    private String getETA()
-    {
-        try
-        {
-            return FunctionHelper.parseTime((int) (todo / speed / FunctionHelper.getTPS()));
-        }
-        catch (Exception e)
-        {
-            return "";
-        }
-    }
+	private String getETA()
+	{
+		try
+		{
+			return FunctionHelper
+					.parseTime((int) (todo / speed / FunctionHelper.getTPS()));
+		} catch (Exception e)
+		{
+			return "";
+		}
+	}
 
-    @Override
-    public void tick()
-    {
-        ticks++;
+	@Override
+	public void tick()
+	{
+		ticks++;
 
-        if (ticks % (20 * 25) == 0)
-        {
-            OutputHandler.chatNotification(source, "Filler for " + dimID + ": " + getStatus());
-        }
+		if (ticks % (20 * 25) == 0)
+		{
+			OutputHandler.chatNotification(source, "Filler for " + dimID + ": "
+					+ getStatus());
+		}
 
-        for (int i = 0; i < speed; i++)
-        {
-			/* skip over chunks that already exist (we use the region cache so
-			 * as to not load them) */
-			while(!RegionFileCache.createOrLoadRegionFile(
-					world.getChunkSaveLocation(), X >> 5, Z >> 5)
-					.chunkExists(X & 0x1F, Z & 0x1F))
+		for (int i = 0; i < speed; i++)
+		{
+			/*
+			 * skip over chunks that already exist (we use the region cache so
+			 * as to not load them)
+			 */
+			while (!RegionFileCache.createOrLoadRegionFile(
+					world.getChunkSaveLocation(), X >> 5, Z >> 5).chunkExists(
+					X & 0x1F, Z & 0x1F))
 			{
 				--todo;
 				if (next())
 					return;
 			}
-			
-			/* get and populate the chunk manually so that it does not get
-			 * fully loaded in, this saves ram and increases performance */
-			ChunkProviderServer provider =
-					(ChunkProviderServer)world.getChunkProvider();
-			
+
+			/*
+			 * get and populate the chunk manually so that it does not get fully
+			 * loaded in, this saves ram and increases performance
+			 */
+			ChunkProviderServer provider = (ChunkProviderServer) world
+					.getChunkProvider();
+
 			Chunk chunk = provider.currentChunkProvider.loadChunk(X, Z);
 			chunk.populateChunk(provider, provider, X, Z);
 			provider.safeSaveChunk(chunk);
-			
+
 			--todo;
-            if (next())
-                return;
-        }
-    }
+			if (next())
+				return;
+		}
+	}
 
-    private boolean next()
-    {
-        // 1 = square
-        if (border.shapeByte == 1)
-        {
-            if (X <= maxX)
-            {
-                X++;
-            }
-            else
-            {
-                if (Z <= maxZ)
-                {
-                    X = minX;
-                    Z++;
-                }
-                else
-                {
-                    isComplete = true;
-                    return true;
-                }
-            }
-        }
-        // 2 = round
-        else if (border.shapeByte == 2)
-        {
-            while (true)
-            {
-                if (X <= maxX)
-                {
-                    X++;
-                }
-                else
-                {
-                    if (Z <= maxZ)
-                    {
-                        X = minX;
-                        Z++;
-                    }
-                    else
-                    {
-                        isComplete = true;
-                        return true;
-                    }
-                }
+	private boolean next()
+	{
+		// 1 = square
+		if (border.shapeByte == 1)
+		{
+			if (X <= maxX)
+			{
+				X++;
+			} else
+			{
+				if (Z <= maxZ)
+				{
+					X = minX;
+					Z++;
+				} else
+				{
+					isComplete = true;
+					return true;
+				}
+			}
+		}
+		// 2 = round
+		else if (border.shapeByte == 2)
+		{
+			while (true)
+			{
+				if (X <= maxX)
+				{
+					X++;
+				} else
+				{
+					if (Z <= maxZ)
+					{
+						X = minX;
+						Z++;
+					} else
+					{
+						isComplete = true;
+						return true;
+					}
+				}
 
-                if (rad >= ModuleWorldBorder.getDistanceRound(centerX, centerZ, X, Z))
-                {
-                    break;
-                }
-            }
-        }
-        else
-        {
-            isComplete = true;
-            throw new RuntimeException("WTF?" + border.shapeByte);
-        }
-        
-        return false;
-    }
+				if (rad >= ModuleWorldBorder.getDistanceRound(centerX, centerZ,
+						X, Z))
+				{
+					break;
+				}
+			}
+		} else
+		{
+			isComplete = true;
+			throw new RuntimeException("WTF?" + border.shapeByte);
+		}
 
-    @Override
-    public void onComplete()
-    {
-        try
-        {
-            boolean var6 = world.levelSaving;
-            world.levelSaving = false;
-            world.saveAllChunks(true, (IProgressUpdate) null);
-            world.levelSaving = var6;
-        }
-        catch (MinecraftException var7)
-        {
-        }
-        if (!stopped)
-        {
-            OutputHandler.chatWarning(source, "Filler finished after " + ticks + " ticks.");
-            System.out.print("Removed filler? :" + DataManager.getInstance().delete(TickTaskFill.class, dimID));
-            System.out.print("Removed filler? :" + DataStorageManager.getReccomendedDriver().deleteObject(con, dimID));
-        }
-        CommandFiller.map.remove(Integer.parseInt(dimID));
-        FEChunkLoader.instance().unforceLoadWorld(world);
-        System.gc();
-    }
+		return false;
+	}
 
-    @Override
-    public boolean isComplete()
-    {
-        return isComplete;
-    }
+	@Override
+	public void onComplete()
+	{
+		try
+		{
+			boolean var6 = world.levelSaving;
+			world.levelSaving = false;
+			world.saveAllChunks(true, (IProgressUpdate) null);
+			world.levelSaving = var6;
+		} catch (MinecraftException var7)
+		{
+		}
+		if (!stopped)
+		{
+			OutputHandler.chatWarning(source, "Filler finished after " + ticks
+					+ " ticks.");
+			System.out.print("Removed filler? :"
+					+ DataManager.getInstance().delete(TickTaskFill.class,
+							dimID));
+			System.out.print("Removed filler? :"
+					+ DataStorageManager.getReccomendedDriver().deleteObject(
+							con, dimID));
+		}
+		CommandFiller.map.remove(Integer.parseInt(dimID));
+		FEChunkLoader.instance().unforceLoadWorld(world);
+		System.gc();
+	}
 
-    @Override
-    public boolean editsBlocks()
-    {
-        return true;
-    }
+	@Override
+	public boolean isComplete()
+	{
+		return isComplete;
+	}
 
-    public void stop()
-    {
-        stopped = true;
-        isComplete = true;
-        DataManager.getInstance().save(this, dimID);
-        DataStorageManager.getReccomendedDriver().saveObject(con, this);
-        OutputHandler.chatWarning(source, "Filler stopped after " + ticks + " ticks. Still to do: " + todo + " chuncks.");
-        System.gc();
-    }
+	@Override
+	public boolean editsBlocks()
+	{
+		return true;
+	}
 
-    public String getStatus()
-    {
-        return "Todo: " + getETA() + " at " + speed + " chuncks per ticks.";
-    }
+	public void stop()
+	{
+		stopped = true;
+		isComplete = true;
+		DataManager.getInstance().save(this, dimID);
+		DataStorageManager.getReccomendedDriver().saveObject(con, this);
+		OutputHandler.chatWarning(source, "Filler stopped after " + ticks
+				+ " ticks. Still to do: " + todo + " chuncks.");
+		System.gc();
+	}
 
-    public void setSpeed(int speed)
-    {
-        this.speed = speed;
-        OutputHandler.chatWarning(source, "Changed speed of filler " + dimID + " to " + speed);
-    }
+	public String getStatus()
+	{
+		return "Todo: " + getETA() + " at " + speed + " chuncks per ticks.";
+	}
+
+	public void setSpeed(int speed)
+	{
+		this.speed = speed;
+		OutputHandler.chatWarning(source, "Changed speed of filler " + dimID
+				+ " to " + speed);
+	}
 }
