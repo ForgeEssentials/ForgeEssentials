6ba8f612fc086af3063c8b3a6c7e905ec398669c
luacs1998 <luacs1998@users.noreply.github.com>
2015-05-23 16:15:26 +0800

    Add package-info for remote and econ APIs

---
+++ src/main/java/com/forgeessentials/api/economy/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = "ForgeEssentials", provides = "ForgeEssentialsAPI|Economy", apiVersion = "2.0")
+
+package com.forgeessentials.api.economy;
+
+import cpw.mods.fml.common.API;
---
+++ src/main/java/com/forgeessentials/api/remote/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = "ForgeEssentials", provides = "ForgeEssentialsAPI|Remote", apiVersion = "2.0")
+
+package com.forgeessentials.api.remote;
+
+import cpw.mods.fml.common.API;
