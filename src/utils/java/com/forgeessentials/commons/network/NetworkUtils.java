04963f2cc7c461f4f6d05097d2bea111327b5866
luacs1998 <luacs1998@users.noreply.github.com>
2015-05-24 23:34:31 +0800

    Register null-handlers differently - fixes a crash

--- src/utils/java/com/forgeessentials/commons/network/NetworkUtils.java
+++ src/utils/java/com/forgeessentials/commons/network/NetworkUtils.java
@@ -13,27 +13,52 @@ public class NetworkUtils
 {
     public static SimpleNetworkWrapper netHandler = NetworkRegistry.INSTANCE.newSimpleChannel("forgeessentials");
 
-    public static class NullMessageHandler implements IMessageHandler<IMessage, IMessage>
-    {
-
-        @Override
-        public IMessage onMessage(IMessage message, MessageContext ctx)
-        {
-            return null;
-        }
-    }
-
     public static void initClientNullHandlers()
     {
-        netHandler.registerMessage(new NullMessageHandler(), Packet0Handshake.class, 0, SERVER);
+        netHandler.registerMessage(new IMessageHandler<Packet0Handshake, IMessage>()
+        {
+            @Override
+            public IMessage onMessage(Packet0Handshake message, MessageContext ctx)
+            {
+                return null;
+            }
+        }, Packet0Handshake.class, 0, SERVER);
     }
 
     public static void initServerNullHandlers()
     {
-        netHandler.registerMessage(new NullMessageHandler(), Packet1SelectionUpdate.class, 1, CLIENT);
-        netHandler.registerMessage(new NullMessageHandler(), Packet5Noclip.class, 5, CLIENT);
-        netHandler.registerMessage(new NullMessageHandler(), Packet6Speed.class, 6, CLIENT);
-        netHandler.registerMessage(new NullMessageHandler(), Packet7Remote.class, 7, CLIENT);
+        netHandler.registerMessage(new IMessageHandler<Packet1SelectionUpdate, IMessage>()
+        {
+            @Override
+            public IMessage onMessage(Packet1SelectionUpdate message, MessageContext ctx)
+            {
+                return null;
+            }
+        }, Packet1SelectionUpdate.class, 1, CLIENT);
+        netHandler.registerMessage(new IMessageHandler<Packet5Noclip, IMessage>()
+        {
+            @Override
+            public IMessage onMessage(Packet5Noclip message, MessageContext ctx)
+            {
+                return null;
+            }
+        }, Packet5Noclip.class, 5, CLIENT);
+        netHandler.registerMessage(new IMessageHandler<Packet6Speed, IMessage>()
+        {
+            @Override
+            public IMessage onMessage(Packet6Speed message, MessageContext ctx)
+            {
+                return null;
+            }
+        }, Packet6Speed.class, 6, CLIENT);
+        netHandler.registerMessage(new IMessageHandler<Packet7Remote, IMessage>()
+        {
+            @Override
+            public IMessage onMessage(Packet7Remote message, MessageContext ctx)
+            {
+                return null;
+            }
+        }, Packet7Remote.class, 7, CLIENT);
 
     }
 }
