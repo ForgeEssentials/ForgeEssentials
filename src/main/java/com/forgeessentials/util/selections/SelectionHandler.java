e3fb21461284a057d769eea566d17ed92ef151cb
Björn Zeutzheim <bjoern@zeutzheim-boppard.de>
2015-05-28 11:27:34 +0900

    Fix for selection updates

--- src/main/java/com/forgeessentials/util/selections/SelectionHandler.java
+++ src/main/java/com/forgeessentials/util/selections/SelectionHandler.java
@@ -1,11 +1,13 @@
 package com.forgeessentials.util.selections;
 
 import com.forgeessentials.commons.network.Packet1SelectionUpdate;
+
 import net.minecraft.entity.player.EntityPlayerMP;
 
 import com.forgeessentials.commons.network.NetworkUtils;
 import com.forgeessentials.core.moduleLauncher.ModuleLauncher;
 import com.forgeessentials.util.OutputHandler;
+import com.forgeessentials.util.PlayerInfo;
 
 public class SelectionHandler
 {
@@ -31,7 +33,17 @@ public class SelectionHandler
 
     public static void sendUpdate(EntityPlayerMP player)
     {
-        NetworkUtils.netHandler.sendTo(new Packet1SelectionUpdate(selectionProvider.getSelection(player)), player);
+        if (PlayerInfo.get(player).getHasFEClient())
+        {
+            try
+            {
+                NetworkUtils.netHandler.sendTo(new Packet1SelectionUpdate(selectionProvider.getSelection(player)), player);
+            }
+            catch (NullPointerException e)
+            {
+                OutputHandler.felog.severe("Error sending selection update to player");
+            }
+        }
     }
 
 }
