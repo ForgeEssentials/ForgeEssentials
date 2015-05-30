52283be80b2ed0fe71ebbd8aa8223326750bb27c
luacs1998 <luacs1998@users.noreply.github.com>
2015-05-30 23:18:04 +0800

    Add config reload event and removed experimental warning from /feinfo reload

---
+++ src/main/java/com/forgeessentials/util/events/ConfigReloadEvent.java
@@ -0,0 +1,12 @@
+package com.forgeessentials.util.events;
+
+import cpw.mods.fml.common.eventhandler.Event;
+
+public class ConfigReloadEvent extends Event
+{
+
+    public ConfigReloadEvent()
+    {
+        super();
+    }
+}
