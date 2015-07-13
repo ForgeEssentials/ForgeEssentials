a17b9ce6b94ca67f0d37fa9ae39fdd0c69c328ff
luacs1998 <luacs1998@users.noreply.github.com>
2015-07-13 11:42:39 +0800

    Added mechanism to enable portal mixin based on environment properties

--- src/main/java/com/forgeessentials/core/preloader/asm/EventInjector.java
+++ src/main/java/com/forgeessentials/core/preloader/asm/EventInjector.java
@@ -16,6 +16,8 @@ public class EventInjector implements IMixinConfigPlugin
 
     public static List<String> injectedPatches = new ArrayList<String>();
 
+    public static final String enableDevMixins = "forgeessentials.developermode.mixin";
+
     @Override
     public void onLoad(String mixinPackage)
     {
@@ -31,6 +33,11 @@ public class EventInjector implements IMixinConfigPlugin
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName)
     {
+        if (mixinClassName.contains("Portal"))
+        {
+            return Boolean.parseBoolean(System.getProperty(enableDevMixins));
+        }
+
         return true;
     }
 
