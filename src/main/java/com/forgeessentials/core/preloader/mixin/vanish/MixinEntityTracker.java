5543e0c75f39eb9d82cb2c5d5919b36f2c4bed29
Bjoern Zeutzheim <bjoern@zeutzheim-boppard.de>
2015-07-28 11:14:07 +0900

    Added /vanish (basically just fakes player as dead)

---
+++ src/main/java/com/forgeessentials/core/preloader/mixin/vanish/MixinEntityTracker.java
@@ -0,0 +1,24 @@
+package com.forgeessentials.core.preloader.mixin.vanish;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityTracker;
+import net.minecraft.entity.EntityTrackerEntry;
+import net.minecraft.util.IntHashMap;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(EntityTracker.class)
+public abstract class MixinEntityTracker implements EntityTrackerHelper
+{
+
+    @Shadow
+    private IntHashMap trackedEntityIDs = new IntHashMap();
+
+    @Override
+    public EntityTrackerEntry getEntityTrackerEntry(Entity entity)
+    {
+        return (EntityTrackerEntry) trackedEntityIDs.lookup(entity.getEntityId());
+    }
+
+}
