a0d3ce17fc69148386ecd38cc5a15a6c8e61948d
Björn Zeutzheim <bjoern@zeutzheim-boppard.de>
2015-08-23 02:27:43 +0900

    Fixed #1698

--- src/main/java/com/forgeessentials/commands/item/CommandCraft.java
+++ src/main/java/com/forgeessentials/commands/item/CommandCraft.java
@@ -1,15 +1,41 @@
 package com.forgeessentials.commands.item;
 
+import java.lang.ref.WeakReference;
+
 import net.minecraft.block.BlockWorkbench;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerOpenContainerEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.permission.PermissionLevel;
 
+import com.forgeessentials.api.permissions.FEPermissions;
 import com.forgeessentials.commands.util.FEcmdModuleCommands;
+import com.forgeessentials.core.misc.TranslatedCommandException;
 
 public class CommandCraft extends FEcmdModuleCommands
 {
+
+    protected WeakReference<EntityPlayer> lastPlayer = new WeakReference<>(null);
+
+    public CommandCraft()
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void playerOpenContainerEvent(PlayerOpenContainerEvent event)
+    {
+        if (event.canInteractWith == false && lastPlayer.get() == event.entityPlayer)
+        {
+            event.setResult(Result.ALLOW);
+        }
+    }
+
     @Override
     public String getCommandName()
     {
@@ -20,12 +46,14 @@ public class CommandCraft extends FEcmdModuleCommands
     public void processCommandPlayer(EntityPlayerMP sender, String[] args) throws CommandException
     {
         EntityPlayerMP player = sender;
-        player.displayGui(new BlockWorkbench.InterfaceCraftingTable(player.worldObj, player.playerLocation));
+        player.displayGui(new BlockWorkbench.InterfaceCraftingTable(player.worldObj, player.getPosition()));
+        lastPlayer = new WeakReference<EntityPlayer>(player);
     }
 
     @Override
     public void processCommandConsole(ICommandSender sender, String[] args) throws CommandException
     {
+        throw new TranslatedCommandException(FEPermissions.MSG_NO_CONSOLE_COMMAND);
     }
 
     @Override
@@ -45,4 +73,5 @@ public class CommandCraft extends FEcmdModuleCommands
     {
         return "/craft Open a crafting window.";
     }
+
 }
