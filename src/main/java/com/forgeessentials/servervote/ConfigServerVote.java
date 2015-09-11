b28eb2f0237a0c139a32d61804c4ffb2154b5c79
liach <liach@users.noreply.github.com>
2015-09-11 12:34:11 -0700

    It should fix it now
    Actually fix that freaking issue

--- src/main/java/com/forgeessentials/servervote/ConfigServerVote.java
+++ src/main/java/com/forgeessentials/servervote/ConfigServerVote.java
@@ -82,17 +82,18 @@ public class ConfigServerVote extends ConfigLoaderBase
             }
 
             Item item = GameData.getItemRegistry().getObject(temp);
-
+            
+            if (item == null) {
+                LoggingHandler.felog.warn("Found invalid item:" + temp);
+                continue;
+            }
+            
             ItemStack stack = new ItemStack(item, amount, meta);
             
             if (stack == null) continue;
 
-            LoggingHandler.felog.debug(stack.getUnlocalizedName());
-
-            if (stack != null)
-            {
-                freeStuff.add(stack);
-            }
+            LoggingHandler.felog.debug("Added reward item stack: " + stack.getUnlocalizedName());
+            freeStuff.add(stack);
         }
 
         loadKeys();
