733c05ec317a72113cfa3bf29fc992df2edbd5cf
Björn Zeutzheim <bjoern@zeutzheim-boppard.de>
2015-09-13 22:07:48 +0900

    Patched zone command permission to use scripting parser

--- src/main/java/com/forgeessentials/protection/effect/CommandEffect.java
+++ src/main/java/com/forgeessentials/protection/effect/CommandEffect.java
@@ -1,7 +1,8 @@
 package com.forgeessentials.protection.effect;
 
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
+
+import com.forgeessentials.scripting.ScriptParser;
 
 public class CommandEffect extends ZoneEffect
 {
@@ -11,18 +12,13 @@ public class CommandEffect extends ZoneEffect
     public CommandEffect(EntityPlayerMP player, int interval, String command)
     {
         super(player, interval, false);
-        command = command.replaceAll("%p", player.getCommandSenderName());
-        command = command.replaceAll("%u", player.getPersistentID().toString());
-        command = command.replaceAll("%x", Integer.toString((int) Math.floor(player.posX)));
-        command = command.replaceAll("%y", Integer.toString((int) Math.floor(player.posY)));
-        command = command.replaceAll("%z", Integer.toString((int) Math.floor(player.posZ)));
         this.command = command;
     }
 
     @Override
     public void execute()
     {
-        MinecraftServer.getServer().getCommandManager().executeCommand(MinecraftServer.getServer(), command);
+        ScriptParser.run(command, player, null);
     }
 
 }
