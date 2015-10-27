dd1ace1d6a19e83378ab4d497d1ad0354785c391
Björn Zeutzheim <bjoern@zeutzheim-boppard.de>
2015-10-27 23:33:15 +0900

    Fixed infinite recursion by NoPlayerInfoEvent. Fixes #1835

--- src/main/java/com/forgeessentials/util/PlayerInfo.java
+++ src/main/java/com/forgeessentials/util/PlayerInfo.java
@@ -129,19 +129,23 @@ public class PlayerInfo implements Loadable
     public static PlayerInfo get(UUID uuid)
     {
         PlayerInfo info = playerInfoMap.get(uuid);
-        if (info == null)
+        if (info != null)
+            return info;
+
+        // Attempt to populate this info with some data from our storage
+        info = DataManager.getInstance().load(PlayerInfo.class, uuid.toString());
+        if (info != null)
         {
-            // Attempt to populate this info with some data from our storage.
-            info = DataManager.getInstance().load(PlayerInfo.class, uuid.toString());
-            if (info == null)
-            {
-                EntityPlayerMP player = UserIdent.getPlayerByUuid(uuid);
-                if (player != null)
-                    APIRegistry.getFEEventBus().post(new NoPlayerInfoEvent(player));
-                info = new PlayerInfo(uuid);
-            }
             playerInfoMap.put(uuid, info);
+            return info;
         }
+
+        // Create new player info data
+        EntityPlayerMP player = UserIdent.getPlayerByUuid(uuid);
+        info = new PlayerInfo(uuid);
+        playerInfoMap.put(uuid, info);
+        if (player != null)
+            APIRegistry.getFEEventBus().post(new NoPlayerInfoEvent(player));
         return info;
     }
 
@@ -395,9 +399,11 @@ public class PlayerInfo implements Loadable
             // ChatOutputHandler.felog.info(String.format("Changing inventory group for %s from %s to %s",
             // ident.getUsernameOrUUID(), activeInventoryGroup, name));
             /*
-             * ChatOutputHandler.felog.info("Items in old inventory:"); for (int i = 0; i < ident.getPlayer().inventory.getSizeInventory(); i++) { ItemStack itemStack =
-             * ident.getPlayer().inventory.getStackInSlot(i); if (itemStack != null) ChatOutputHandler.felog.info("  " + itemStack.getDisplayName()); }
-             * ChatOutputHandler.felog.info("Items in new inventory:"); for (ItemStack itemStack : newInventory) if (itemStack != null) ChatOutputHandler.felog.info("  " +
+             * ChatOutputHandler.felog.info("Items in old inventory:"); for (int i = 0; i <
+             * ident.getPlayer().inventory.getSizeInventory(); i++) { ItemStack itemStack =
+             * ident.getPlayer().inventory.getStackInSlot(i); if (itemStack != null) ChatOutputHandler.felog.info("  " +
+             * itemStack.getDisplayName()); } ChatOutputHandler.felog.info("Items in new inventory:"); for (ItemStack
+             * itemStack : newInventory) if (itemStack != null) ChatOutputHandler.felog.info("  " +
              * itemStack.getDisplayName());
              */
 
