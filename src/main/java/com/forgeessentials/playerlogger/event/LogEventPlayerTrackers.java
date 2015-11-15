5b04d1e15d5d345eac1736321861428413822c9f
luacs1998 <luacs1998@users.noreply.github.com>
2015-11-15 22:01:31 +0800

    olee says I should do things differently

--- src/main/java/com/forgeessentials/playerlogger/event/LogEventPlayerTrackers.java
+++ src/main/java/com/forgeessentials/playerlogger/event/LogEventPlayerTrackers.java
@@ -5,93 +5,57 @@ import javax.persistence.EntityManager;
 import com.forgeessentials.playerlogger.PlayerLoggerEvent;
 import com.forgeessentials.playerlogger.entity.Action;
 
-import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerChangedDimensionEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 
-public class LogEventPlayerTrackers
+public class LogEventPlayerTrackers extends PlayerLoggerEvent<PlayerEvent>
 {
-    public static class Login extends PlayerLoggerEvent<PlayerLoggedInEvent>
+    public LogEventPlayerTrackers(PlayerEvent event)
     {
-        public Login(PlayerLoggedInEvent e)
+        super(event);
+    }
+
+    @Override
+    public void process(EntityManager em)
+    {
+        Action action = new Action();
+
+        action.time = date;
+        action.player = getPlayer(event.player.getPersistentID());
+        action.world = getWorld(event.player.worldObj.provider.dimensionId);
+        action.x = (int) event.player.posX;
+        action.y = (int) event.player.posY;
+        action.z = (int) event.player.posZ;
+    }
+
+    public static class Login extends LogEventPlayerTrackers
+    {
+        public Login(PlayerEvent.PlayerLoggedInEvent e)
         {
             super(e);
-        }
-
-        @Override
-        public void process(EntityManager em)
-        {
-            Action action = new Action();
-
-            action.time = date;
-            action.player = getPlayer(event.player.getPersistentID());
-            action.world = getWorld(event.player.worldObj.provider.dimensionId);
-            action.x = (int) event.player.posX;
-            action.y = (int) event.player.posY;
-            action.z = (int) event.player.posZ;
         }
     }
 
-    public static class Logout extends PlayerLoggerEvent<PlayerLoggedOutEvent>
+    public static class Logout extends LogEventPlayerTrackers
     {
-        public Logout(PlayerLoggedOutEvent e)
+        public Logout(PlayerEvent.PlayerLoggedOutEvent e)
         {
             super(e);
-        }
-
-        @Override
-        public void process(EntityManager em)
-        {
-            Action action = new Action();
-
-            action.time = date;
-            action.player = getPlayer(event.player.getPersistentID());
-            action.world = getWorld(event.player.worldObj.provider.dimensionId);
-            action.x = (int) event.player.posX;
-            action.y = (int) event.player.posY;
-            action.z = (int) event.player.posZ;
-        }
-    }
-    public static class Respawn extends PlayerLoggerEvent<PlayerRespawnEvent>
-    {
-        public Respawn(PlayerRespawnEvent e)
-        {
-            super(e);
-        }
-
-        @Override
-        public void process(EntityManager em)
-        {
-            Action action = new Action();
-
-            action.time = date;
-            action.player = getPlayer(event.player.getPersistentID());
-            action.world = getWorld(event.player.worldObj.provider.dimensionId);
-            action.x = (int) event.player.posX;
-            action.y = (int) event.player.posY;
-            action.z = (int) event.player.posZ;
         }
     }
 
-    public static class ChangeDim extends PlayerLoggerEvent<PlayerChangedDimensionEvent>
+    public static class Respawn extends LogEventPlayerTrackers
     {
-        public ChangeDim(PlayerChangedDimensionEvent e)
+        public Respawn(PlayerEvent.PlayerRespawnEvent e)
         {
             super(e);
         }
+    }
 
-        @Override
-        public void process(EntityManager em)
+    public static class ChangeDim extends LogEventPlayerTrackers
+    {
+        public ChangeDim(PlayerEvent.PlayerChangedDimensionEvent e)
         {
-            Action action = new Action();
-
-            action.time = date;
-            action.player = getPlayer(event.player.getPersistentID());
-            action.world = getWorld(event.toDim);
-            action.x = (int) event.player.posX;
-            action.y = (int) event.player.posY;
-            action.z = (int) event.player.posZ;
+            super(e);
         }
     }
 }
