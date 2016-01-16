e18e35123062ca83347ef643402819cd00b70d06
luacs1998 <luacs1998@users.noreply.github.com>
2016-01-16 09:46:25 +0800

    Disable debug printlns - fix #1953

--- src/main/java/com/forgeessentials/core/preloader/injections/MixinBlockFire.java
+++ src/main/java/com/forgeessentials/core/preloader/injections/MixinBlockFire.java
@@ -24,7 +24,7 @@ public abstract class MixinBlockFire extends BlockFire
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlock(IIILnet/minecraft/block/Block;II)Z", shift = Shift.LAST_LABEL) )
     public void handleTryCatchFireA(World world, int x, int y, int z, int chance, Random rnd, int argValue1, ForgeDirection face, CallbackInfo ci)
     {
-        System.out.println("Injector: Fire destroyed block and spread to below block");
+        //System.out.println("Injector: Fire destroyed block and spread to below block");
         if (MinecraftForge.EVENT_BUS.post(new FireEvent.Destroy(x, y, z, world, this, argValue1)))
         {
             ci.doReturn();
@@ -36,7 +36,7 @@ public abstract class MixinBlockFire extends BlockFire
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockToAir(III)Z", shift = Shift.LAST_LABEL) )
     public void handleTryCatchFireB(World world, int x, int y, int z, int chance, Random rnd, int argValue1, ForgeDirection face, CallbackInfo ci)
     {
-        System.out.println("Injector: Fire destroyed block");
+        //System.out.println("Injector: Fire destroyed block");
         if (MinecraftForge.EVENT_BUS.post(new FireEvent.Destroy(x, y, z, world, this, argValue1)))
         {
             ci.doReturn();
@@ -49,7 +49,7 @@ public abstract class MixinBlockFire extends BlockFire
     public void updateTick(World world, int sourceX, int sourceY, int sourceZ, Random rnd, CallbackInfo ci, @Local("p_149674_2_") int x,
             @Local("p_149674_4_") int y, @Local("p_149674_3_") int z)
     {
-        System.out.println(String.format("Injector: Fire spreading to other block from [%d,%d,%d] to [%d,%d,%d]", sourceX, sourceY, sourceZ, x, y, z));
+        //System.out.println(String.format("Injector: Fire spreading to other block from [%d,%d,%d] to [%d,%d,%d]", sourceX, sourceY, sourceZ, x, y, z));
         if (MinecraftForge.EVENT_BUS.post(new FireEvent.Spread(x, y, z, world, this, 3, sourceX, sourceY, sourceZ)))
         {
             ci.doReturn();
