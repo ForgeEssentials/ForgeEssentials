a2e88b6a320d9fa4f7767effcebc0b7adfaef873
luacs1998 <luacs1998@users.noreply.github.com>
2016-08-25 13:43:11 +0800

    Fix compile issues, add new mixins to json

--- src/main/java/com/forgeessentials/core/preloader/mixin/block/MixinBlockEndPortal.java
+++ src/main/java/com/forgeessentials/core/preloader/mixin/block/MixinBlockEndPortal.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Overwrite;
 
 import net.minecraft.block.BlockEndPortal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
@@ -16,7 +17,7 @@ public class MixinBlockEndPortal
 {
 
     @Overwrite
-    public void onEntityCollidedWithBlock(World p_149670_1_, BlockPos pos, Entity p_149670_5_)
+    public void onEntityCollidedWithBlock(World p_149670_1_, BlockPos pos, IBlockState state, Entity p_149670_5_)
     { // TODO: get target coordinates somehow
         if (p_149670_5_.ridingEntity == null && p_149670_5_.riddenByEntity == null && !p_149670_1_.isRemote && !MinecraftForge.EVENT_BUS.post(new EntityPortalEvent(p_149670_5_, p_149670_1_, pos, 1, new BlockPos(0, 0, 0))))
         {
