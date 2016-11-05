ecd3cbf30e777a07c8f4efb5bf4c30dd2eb3fa2a
olee <bjoern@zeutzheim-boppard.de>
2016-11-05 16:03:03 +0100

    Added missing file

---
+++ src/utils/java/net/minecraftforge/permission/PermissionObject.java
@@ -0,0 +1,10 @@
+package net.minecraftforge.permission;
+
+public interface PermissionObject
+{
+
+    public String getPermissionNode();
+
+    public PermissionLevel getPermissionLevel();
+    
+}
