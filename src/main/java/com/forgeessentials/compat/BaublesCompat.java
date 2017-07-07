531f60339ce48830d8a64458cd839a45139f6b45
luacs1998 <luacs1998@users.noreply.github.com>
2017-07-07 09:53:54 +0800

    Fix baubles compatibility issues

--- src/main/java/com/forgeessentials/compat/BaublesCompat.java
+++ src/main/java/com/forgeessentials/compat/BaublesCompat.java
@@ -1,17 +1,25 @@
 package com.forgeessentials.compat;
 
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Optional.Method;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
 import com.forgeessentials.util.events.FEPlayerEvent.InventoryGroupChange;
 import com.forgeessentials.util.events.ServerEventHandler;
+import com.forgeessentials.util.output.LoggingHandler;
 
 import baubles.api.BaublesApi;
 
 public class BaublesCompat extends ServerEventHandler
 {
-    @Method(modid = "baubles")
+    public BaublesCompat()
+    {
+        if (Loader.isModLoaded("Baubles"))
+            LoggingHandler.felog.info("Baubles compatibility enabled.");
+        register();
+    }
+    @Method(modid = "Baubles")
     @SubscribeEvent
     public void updateInventory(InventoryGroupChange e)
     {
