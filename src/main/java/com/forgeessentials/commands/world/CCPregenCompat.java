ccf5ff6f31950ddffff3637b9c062bc695126e10
Spacebuilder2020 <alexander-davis@outlook.com>
2019-04-09 22:16:14 -0600

    Update CC compat to support new versions of CC Part 1

--- src/main/java/com/forgeessentials/commands/world/CCPregenCompat.java
+++ src/main/java/com/forgeessentials/commands/world/CCPregenCompat.java
@@ -1,51 +1,16 @@
 package com.forgeessentials.commands.world;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
 
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 
-import cubicchunks.regionlib.impl.EntryLocation3D;
-import cubicchunks.regionlib.impl.save.SaveSection3D;
-import cubicchunks.regionlib.lib.ExtRegion;
-import cubicchunks.regionlib.lib.provider.SimpleRegionProvider;
 import io.github.opencubicchunks.cubicchunks.api.world.ICubeProviderServer.Requirement;
 import io.github.opencubicchunks.cubicchunks.core.asm.mixin.ICubicWorldInternal.Server;
 import io.github.opencubicchunks.cubicchunks.core.server.CubeProviderServer;
-import io.github.opencubicchunks.cubicchunks.core.server.chunkio.SharedCachedRegionProvider;
 
 public class CCPregenCompat
 {
-    private static SaveSection3D section3D;
-    private static World world;
-    public static SaveSection3D getSavedSection3D(World world)
-    {
-        if (section3D == null || CCPregenCompat.world != world)
-        {
-            String folder = world.provider.getSaveFolder();
-            if (folder == null)
-            {
-                folder = ".";
-            }
-            Path part3d = Paths.get(folder, "region3d");
-
-            section3D = new SaveSection3D(
-                    new SharedCachedRegionProvider<>(
-                            SimpleRegionProvider.createDefault(new EntryLocation3D.Provider(), part3d, 512)
-                    ),
-                    new SharedCachedRegionProvider<>(
-                            new SimpleRegionProvider<>(new EntryLocation3D.Provider(), part3d,
-                                    (keyProvider, regionKey) -> new ExtRegion<>(part3d, Collections.emptyList(), keyProvider, regionKey)
-                            )
-                    ));
-            CCPregenCompat.world = world;
-        }
-
-        return section3D;
-    }
 
     public static boolean isCCWorld(World world)
     {
@@ -57,19 +22,16 @@ public class CCPregenCompat
 
         CubeProviderServer providerServer = (CubeProviderServer) provider;
         Server worldInternal = (Server) world;
-
-        try
+        if (providerServer.isCubeGenerated(x,y,z))
         {
-            if (getSavedSection3D(world).hasEntry(new EntryLocation3D(x, y, z)) || providerServer.getCube(x, y, z, Requirement.LOAD) != null)
-            {
-                return false;
-            }
-        } catch (IOException e) {}
+            return false;
+        }
 
         if (providerServer.getLoadedCubeCount() > 256)
         {
             providerServer.saveChunks(true);
-            worldInternal.getChunkGarbageCollector().chunkGc();
+            //TODO: Replace/update this line with code that replicates the functionality of providerServer.queueUnloadAll() in CC
+            //worldInternal.getChunkGarbageCollector().chunkGc();
         }
 
         providerServer.getCube(x, y, z, Requirement.LIGHT);
