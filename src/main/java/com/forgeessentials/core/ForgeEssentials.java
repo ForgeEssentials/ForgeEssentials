91256a4926a688f94cb8d14738589c1e2ef9a3f8
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2019-06-25 20:49:35 -0600

    Try again... Load after FTBLib but before FTBU

--- src/main/java/com/forgeessentials/core/ForgeEssentials.java
+++ src/main/java/com/forgeessentials/core/ForgeEssentials.java
@@ -105,7 +105,7 @@ import com.forgeessentials.util.selections.SelectionHandler;
  */
 
 @Mod(modid = ForgeEssentials.MODID, name = "Forge Essentials", version = BuildInfo.BASE_VERSION, acceptableRemoteVersions = "*",
-        dependencies = BuildInfo.DEPENDENCIES + ";after:worldedit;after:ftbutilities")
+        dependencies = BuildInfo.DEPENDENCIES + ";after:worldedit;before:ftbutilities;after:ftblib")
 public class ForgeEssentials extends ConfigLoaderBase
 {
 
