dba5021a3f7d5080bede3761123f456f7b36dfed
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2020-02-20 20:47:10 -0700

    Update Mixin Gradle and remove Cubic Chunks as a dep

--- src/main/java/com/forgeessentials/commands/world/CCPregenCompat.java
+++ src/main/java/com/forgeessentials/commands/world/CCPregenCompat.java
@@ -5,33 +5,25 @@ import java.io.IOException;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 
+import io.github.opencubicchunks.cubicchunks.api.world.ICubeProviderServer;
 import io.github.opencubicchunks.cubicchunks.api.world.ICubeProviderServer.Requirement;
-import io.github.opencubicchunks.cubicchunks.core.asm.mixin.ICubicWorldInternal.Server;
-import io.github.opencubicchunks.cubicchunks.core.server.CubeProviderServer;
+import io.github.opencubicchunks.cubicchunks.api.world.ICubicWorld;
 
 public class CCPregenCompat
 {
 
     public static boolean isCCWorld(World world)
     {
-        return world instanceof Server && ((Server) world).isCubicWorld();
+        return world instanceof ICubicWorld && ((ICubicWorld) world).isCubicWorld();
     }
 
     public static boolean genCube(World world, IChunkProvider provider, int x, int y, int z)
     {
 
-        CubeProviderServer providerServer = (CubeProviderServer) provider;
-        Server worldInternal = (Server) world;
+        ICubeProviderServer providerServer = (ICubeProviderServer) provider;
         if (providerServer.isCubeGenerated(x,y,z))
         {
             return false;
-        }
-
-        if (providerServer.getLoadedCubeCount() > 256)
-        {
-            providerServer.saveChunks(true);
-            //TODO: Replace/update this line with code that replicates the functionality of providerServer.queueUnloadAll() in CC
-            //worldInternal.getChunkGarbageCollector().chunkGc();
         }
 
         providerServer.getCube(x, y, z, Requirement.LIGHT);
