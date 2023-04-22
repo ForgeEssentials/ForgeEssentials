28173d4045685023e399108dc48415e5db79c812
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-04-21 22:19:42 -0500

    Debug vip command

--- src/main/java/com/forgeessentials/auth/CommandVIP.java
+++ src/main/java/com/forgeessentials/auth/CommandVIP.java
@@ -9,6 +9,7 @@ import net.minecraftforge.server.permission.DefaultPermissionLevel;
 import com.forgeessentials.api.APIRegistry;
 import com.forgeessentials.api.UserIdent;
 import com.forgeessentials.core.commands.ForgeEssentialsCommandBuilder;
+import com.forgeessentials.util.output.ChatOutputHandler;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
@@ -47,23 +48,16 @@ public class CommandVIP extends ForgeEssentialsCommandBuilder
     @Override
     public int execute(CommandContext<CommandSource> ctx, String params) throws CommandSyntaxException
     {
-        EntityArgument.getPlayer(ctx, "player").getDisplayName().getString();
-        PlayerEntity arg = null;
-        try
-        {
-            arg = EntityArgument.getPlayer(ctx,"player");
-        }
-        catch (CommandSyntaxException e)
-        {
-            e.printStackTrace();
-        }
+        PlayerEntity arg = EntityArgument.getPlayer(ctx,"player");
         if (params.equals("add"))
         {
             APIRegistry.perms.setPlayerPermission(UserIdent.get(arg), "fe.auth.vip", true);
+            ChatOutputHandler.chatConfirmation(ctx.getSource(), "Player added to vip list");
         }
         else if (params.equals("remove"))
         {
             APIRegistry.perms.setPlayerPermission(UserIdent.get(arg), "fe.auth.vip", false);
+            ChatOutputHandler.chatConfirmation(ctx.getSource(), "Player removed from vip list");
         }
         return Command.SINGLE_SUCCESS;
     }
