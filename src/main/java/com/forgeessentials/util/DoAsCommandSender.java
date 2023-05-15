e90828dad11ad45332a8d190cc44898ca38dac8a
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-05-15 18:52:13 -0500

    Debug DoAsCommandSender message giving

--- src/main/java/com/forgeessentials/util/DoAsCommandSender.java
+++ src/main/java/com/forgeessentials/util/DoAsCommandSender.java
@@ -81,10 +81,7 @@ public class DoAsCommandSender extends FakePlayer
 	@Override
 	public void sendMessage(ITextComponent message, UUID p_145747_2_) {
 		if (!hideChatMessages)
-		    //TODO see which one works
 		    ChatOutputHandler.sendMessageI(sender, message);
-		    getServer().getPlayerList().broadcastMessage(message, ChatType.SYSTEM, p_145747_2_);
-            //sender.sendMessage(message, p_145747_2_);
 		
 	}
 
