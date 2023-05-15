f5d54183ad0725ad41e7b9042d248d79dac4f782
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-05-15 13:51:14 -0500

    Deprecate TranslatedCommandException

--- src/main/java/com/forgeessentials/core/misc/TranslatedCommandException.java
+++ src/main/java/com/forgeessentials/core/misc/TranslatedCommandException.java
@@ -2,14 +2,15 @@ package com.forgeessentials.core.misc;
 
 import net.minecraft.command.CommandException;
 
+@Deprecated
 public class TranslatedCommandException extends CommandException
 {
-
+	@Deprecated
     public TranslatedCommandException(String message)
     {
         super(Translator.translateITC(message));
     }
-
+	@Deprecated
     public TranslatedCommandException(String message, Object... args)
     {
         super(Translator.translateITC(message, args));
