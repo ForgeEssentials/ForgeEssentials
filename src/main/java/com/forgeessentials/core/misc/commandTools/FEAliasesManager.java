64eb351392fd8d10e26edc5981da93a2e112db05
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-07-30 09:30:16 -0500

    Finish AliasManager

--- src/main/java/com/forgeessentials/core/misc/commandTools/FEAliasesManager.java
+++ src/main/java/com/forgeessentials/core/misc/commandTools/FEAliasesManager.java
@@ -5,20 +5,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.forgeessentials.commands.ModuleCommands;
 import com.forgeessentials.core.ForgeEssentials;
-import com.forgeessentials.core.config.ConfigBase;
-import com.forgeessentials.core.config.ConfigData;
 import com.forgeessentials.data.v2.DataManager;
-import com.forgeessentials.serverNetwork.utils.ConnectionData.ConnectedClientData;
-import com.forgeessentials.serverNetwork.utils.ConnectionData.LocalClientData;
-import com.forgeessentials.serverNetwork.utils.ConnectionData.LocalServerData;
-import com.forgeessentials.util.output.logger.LoggingHandler;
 import com.google.gson.annotations.Expose;
-
-import net.minecraftforge.common.ForgeConfigSpec;
 
 public class FEAliasesManager
 {
@@ -27,43 +18,17 @@ public class FEAliasesManager
 		loadData();
 	}
 	public AliasesMap aliasMap = new AliasesMap();
+	private List<String> test = new ArrayList<String>(){{add("gg");}};
 
     public void loadCommandAliases(final FECommandData commandData)
     {
     	if(aliasMap.getList().containsKey(commandData.getName())) {
-    		
+    		//commandData.setAliases(aliasMap.getList().get(commandData.getName()));
+    		commandData.setAliases(test);
+    		System.out.println(commandData.getAliases());
+    		return;
     	}
-        final ForgeConfigSpec.ConfigValue<List<? extends String>> aliases;
-        String commandCat = "Command-" + commandData.getData().getName();
-        // LoggingHandler.felog.info(commandCat);
-
-        // load from command config names
-//        dataA.getSpecBuilder().push(commandCat);
-//        aliases = dataA.getSpecBuilder().defineList("aliases", (commandData.getData().getAliases()),
-//                ConfigBase.stringValidator);
-//        dataA.getSpecBuilder().pop();
-//
-//        commandAlises.put(commandData.getData().getName(), aliases);
-        
-        // LoggingHandler.felog.info(commandData.getData().getName());
-        // load aliases and test for newMappings
-//        List<String> aliasesProperty = new ArrayList<>(commandAlises.get(commandData.getData().getName()).get());
-//        if (ModuleCommands.newMappings)
-//        {
-//            aliasesProperty.clear();
-//            for (String alias : commandData.getData().getAliases())
-//            {
-//                aliasesProperty.add(String.valueOf(alias));
-//            }
-//
-//            final ForgeConfigSpec.ConfigValue<List<? extends String>> string = commandAlises
-//                    .get(commandData.getData().getName());
-//            string.set(aliasesProperty);
-//            commandAlises.put(commandData.getData().getName(), string);
-//        }
-//        LoggingHandler.felog.info(aliasesProperty.toString());
-//        // set aliases
-//        commandData.getData().setAliases(aliasesProperty);
+    	aliasMap.addAliases(commandData.getName(), commandData.getAliases());
     }
 
     private static File getAliasFile()
@@ -90,6 +55,9 @@ public class FEAliasesManager
 		
 		public Map<String, List<String>> getList(){
 			return aliases;
+		}
+		public void addAliases(String name, List<String> alias){
+			aliases.put(name, alias);
 		}
 	}
 }
