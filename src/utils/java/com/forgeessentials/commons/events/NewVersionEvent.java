357d82802b192feb194e124da821b594c0393428
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-07-31 20:32:59 -0500

    BackPort new update checker from 1.16.5

---
+++ src/utils/java/com/forgeessentials/commons/events/NewVersionEvent.java
@@ -0,0 +1,10 @@
+package com.forgeessentials.commons.events;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+public class NewVersionEvent extends Event
+{
+    public NewVersionEvent()
+    {
+    }
+}
