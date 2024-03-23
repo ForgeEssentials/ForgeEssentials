d29986a83a978558efe22bdb748a4fdea199b32f
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2024-03-23 12:47:27 -0600

    Got Client to compile

--- src/client/java/com/forgeessentials/client/handler/Packet03PlayerPermissionsOverlay.java
+++ src/client/java/com/forgeessentials/client/handler/Packet03PlayerPermissionsOverlay.java
@@ -3,14 +3,6 @@ package com.forgeessentials.client.handler;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.lwjgl.opengl.GL11;
-
-import com.forgeessentials.client.ForgeEssentialsClient;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.blaze3d.vertex.VertexFormat;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.core.BlockPos;
@@ -23,11 +15,23 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.PostLayer;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.PreLayer;
+import net.minecraftforge.client.gui.ForgeIngameGui;
+import net.minecraftforge.client.gui.IIngameOverlay;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.LeftClickBlock;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickItem;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import org.lwjgl.opengl.GL11;
+
+import com.forgeessentials.client.ForgeEssentialsClient;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.blaze3d.vertex.VertexFormat;
 
 public class Packet03PlayerPermissionsOverlay extends GuiComponent
 {
@@ -86,48 +90,57 @@ public class Packet03PlayerPermissionsOverlay extends GuiComponent
     @SubscribeEvent
     public void renderGameOverlayEvent(RenderGameOverlayEvent event)
     {
-        if (!event.isCancelable() && event.getType() == ElementType.HOTBAR)
+        if (event.getType() == ElementType.LAYER)
         {
-            Minecraft instance = Minecraft.getInstance();
-            instance.getTextureManager().bindForSetup(deniedBreakTexture);
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            GL11.glDisable(GL11.GL_LIGHTING);
-            GL11.glEnable(GL11.GL_BLEND);
-
-            int width = instance.getWindow().getGuiScaledWidth();
-            int height = instance.getWindow().getGuiScaledHeight();
-
-            for (int i = 0; i < 9; ++i)
-            {
-                ItemStack stack = instance.player.getInventory().getItem(i);
-                if (stack == ItemStack.EMPTY)
-                    continue;
-                String itemId = ForgeRegistries.ITEMS.getKey(stack.getItem()).toString();
-                if (!placeIds.contains(itemId))
-                    continue;
-                int x = width / 2 - 90 + i * 20 + 2;
-                int y = height - 16 - 3;
-                drawTexturedRect(x + 8, y + 1, 8, 8);
+            IIngameOverlay overlay = null;
+            if (event instanceof PreLayer) {
+                overlay = ((PreLayer) event).getOverlay();
+            } else if (event instanceof PostLayer) {
+                overlay = ((PostLayer) event).getOverlay();
             }
-        }
-        else if (event.isCancelable() && event.getType() == ElementType.CROSSHAIRS)
-        {
-            Minecraft instance = Minecraft.getInstance();
-            float width = instance.getWindow().getGuiScaledWidth();
-            float height = instance.getWindow().getGuiScaledHeight();
-            
-            Entity entity = instance.getCameraEntity();
-            HitResult block = entity.pick(instance.player.getAttribute(net.minecraftforge.common.ForgeMod.REACH_DISTANCE.get()).getValue(), 0.0F, false);
-            if (block.getType() == HitResult.Type.BLOCK)
+            if (!event.isCancelable() && overlay == ForgeIngameGui.HOTBAR_ELEMENT)
             {
-            	BlockPos blockpos = ((BlockHitResult)block).getBlockPos();
-                BlockState blockstate = instance.level.getBlockState(blockpos);
-                String blockId = ForgeRegistries.BLOCKS.getKey(blockstate.getBlock()).toString();
-                if (breakIds.contains(blockId))
+                Minecraft instance = Minecraft.getInstance();
+                instance.getTextureManager().bindForSetup(deniedBreakTexture);
+                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                GL11.glDisable(GL11.GL_LIGHTING);
+                GL11.glEnable(GL11.GL_BLEND);
+
+                int width = instance.getWindow().getGuiScaledWidth();
+                int height = instance.getWindow().getGuiScaledHeight();
+
+                for (int i = 0; i < 9; ++i)
                 {
-                    instance.textureManager.bindForSetup(deniedBreakTexture);
-                    drawTexturedRect(width / 2 - 5, height / 2 - 5, 10, 10);
-                    event.setCanceled(true);
+                    ItemStack stack = instance.player.getInventory().getItem(i);
+                    if (stack == ItemStack.EMPTY)
+                        continue;
+                    String itemId = ForgeRegistries.ITEMS.getKey(stack.getItem()).toString();
+                    if (!placeIds.contains(itemId))
+                        continue;
+                    int x = width / 2 - 90 + i * 20 + 2;
+                    int y = height - 16 - 3;
+                    drawTexturedRect(x + 8, y + 1, 8, 8);
+                }
+            }
+            else if (event.isCancelable() && overlay == ForgeIngameGui.CROSSHAIR_ELEMENT)
+            {
+                Minecraft instance = Minecraft.getInstance();
+                float width = instance.getWindow().getGuiScaledWidth();
+                float height = instance.getWindow().getGuiScaledHeight();
+
+                Entity entity = instance.getCameraEntity();
+                HitResult block = entity.pick(instance.player.getAttribute(net.minecraftforge.common.ForgeMod.REACH_DISTANCE.get()).getValue(), 0.0F, false);
+                if (block.getType() == HitResult.Type.BLOCK)
+                {
+                    BlockPos blockpos = ((BlockHitResult) block).getBlockPos();
+                    BlockState blockstate = instance.level.getBlockState(blockpos);
+                    String blockId = ForgeRegistries.BLOCKS.getKey(blockstate.getBlock()).toString();
+                    if (breakIds.contains(blockId))
+                    {
+                        instance.textureManager.bindForSetup(deniedBreakTexture);
+                        drawTexturedRect(width / 2 - 5, height / 2 - 5, 10, 10);
+                        event.setCanceled(true);
+                    }
                 }
             }
         }
