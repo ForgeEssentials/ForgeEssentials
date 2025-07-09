b807266042b7b852ca9ebbae84fcc9f57698cb46
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2025-07-08 21:18:58 -0600

    Remove log spam! (#2713)

--- src/main/java/com/forgeessentials/permissions/ftbu_compat/FTBURankConfigHandler.java
+++ src/main/java/com/forgeessentials/permissions/ftbu_compat/FTBURankConfigHandler.java
@@ -67,13 +67,11 @@ public class FTBURankConfigHandler implements IRankConfigHandler
 
     @Override public ConfigValue getConfigValue(MinecraftServer server, GameProfile profile, String node)
     {
-        LoggingHandler.felog.info("Getting Config value " + node + " for player " + profile.getName());
         ConfigValue value = ConfigNull.INSTANCE;
 
         RankConfigValueInfo info = getInfo(node);
 
         if (info != null) {
-            LoggingHandler.felog.info("Config Value is not null");
             UserIdent ident = UserIdent.get(profile);
             value = info.defaultValue.copy();
             WorldPoint point = null;
@@ -82,11 +80,9 @@ public class FTBURankConfigHandler implements IRankConfigHandler
             }
 
             if (!value.setValueFromString(null, APIRegistry.perms.getUserPermissionProperty(ident, node), false)) {
-                LoggingHandler.felog.info("Failed to set value");
+                LoggingHandler.felog.debug("Failed to set value ({}) for {}", value, node);
                 return ConfigNull.INSTANCE;
             }
-
-            LoggingHandler.felog.info(node+ " set to " + value.getString());
         }
 
         return value;
