67048047655794aba9b035123351158de53816d8
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2026-06-11 22:53:15 -0500

    Fix Missing int tag when serializing a list. (#2722)

--- src/main/java/com/forgeessentials/data/v2/types/NBTTagCompoundType.java
+++ src/main/java/com/forgeessentials/data/v2/types/NBTTagCompoundType.java
@@ -107,6 +107,11 @@ public class NBTTagCompoundType implements DataType<CompoundNBT>
                     for (int i = 0; i < tagList.size(); i++)
                         jsonArray.add(new JsonPrimitive(tagList.getDouble(i)));
                     break;
+                case NBT.TAG_INT:
+                    typeId = "i";
+                    for (int i = 0; i < tagList.size(); i++)
+                        jsonArray.add(new JsonPrimitive(tagList.getInt(i)));
+                    break;
                 case NBT.TAG_STRING:
                     typeId = "S";
                     for (int i = 0; i < tagList.size(); i++)
